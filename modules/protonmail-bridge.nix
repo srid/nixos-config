31aa09f91821cfbb4a0ff82d49d070367da2fdee
Sridhar Ratnakumar <srid@srid.ca>
2021-04-06 22:27:17 -0400

    Start protonmail-bridge as a systemd service

--- modules/protonmail-bridge.nix
+++ modules/protonmail-bridge.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }: {
   environment.systemPackages = [ pkgs.protonmail-bridge ];
   services.gnome3.gnome-keyring.enable = true;
+
+  # Before starting the service, use `protonmail-bridge --cli` and run 'login'
+  # to configure.
+  systemd.user.services.protonmail-bridge = {
+    description = "Protonmail Bridge";
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.pass ];
+    serviceConfig = {
+      Restart = "always";
+      ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
+    };
+  };
 }
