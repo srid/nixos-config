07da357a6ce4d83cbabb469bdb9fb50e0c85889b
Sridhar Ratnakumar <srid@srid.ca>
2021-04-07 18:20:52 -0400

    Add himalaya to NixOS
    Uses https://github.com/soywod/himalaya/pull/73

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
-  description = "NixOS configuration";
+  description = "Srid's NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    himalaya.url = "github:srid/himalaya/nixify";
   };
 
-  outputs = { self, home-manager, nixpkgs }: 
+  outputs = inputs@{ self, home-manager, nixpkgs, ... }: 
     let 
+      system = "x86_64-linux";
       # Make configuration for any computer I use in my home office.
       mkHomeMachine = configurationNix: nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [
           # System configuration
           configurationNix
@@ -18,6 +20,23 @@
           ./modules/syncthing.nix
           ./modules/protonmail-bridge.nix
           ./modules/monitor-brightness.nix
+
+          # Packages from flake inputs
+          ({ pkgs, lib, ... }: {
+            environment.systemPackages = 
+              let 
+                # Wrap himalaya to be aware of ProtonMail's bridge cert.
+                himalaya = lib.getAttrFromPath [system] inputs.himalaya.defaultPackage;
+                himalayaWithSslEnv = 
+                  pkgs.writeScriptBin "h" ''
+                    #!${pkgs.stdenv.shell}
+                    export SSL_CERT_FILE=~/.config/protonmail/bridge/cert.pem
+                    exec ${himalaya}/bin/himalaya
+                    '';
+              in [
+                himalayaWithSslEnv
+              ];
+          })
 
           # home-manager configuration
           home-manager.nixosModules.home-manager
