3e47c41cdd5ea47cd26a22ad6085c9617936607e
Sridhar Ratnakumar <srid@srid.ca>
2021-04-07 19:59:20 -0400

    Use himalaya direct; flake has issues

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    himalaya.url = "github:srid/himalaya/nixify";
+    himalaya = {
+      url = "github:srid/himalaya/nixify";
+    };
   };
 
   outputs = inputs@{ self, home-manager, nixpkgs, ... }:
@@ -23,16 +25,16 @@
 
           # Packages from flake inputs
           ({ pkgs, lib, ... }: {
-
             environment.systemPackages =
               let
+                # FIXME: This leads to forbidden error
+                # himalaya = toString inputs.himalaya.defaultApp."${system}".program;
                 # Wrap himalaya to be aware of ProtonMail's bridge cert.
-                himalaya = lib.getAttrFromPath [ system ] inputs.himalaya.defaultPackage;
                 himalayaWithSslEnv =
                   pkgs.writeScriptBin "h" ''
                     #!${pkgs.stdenv.shell}
                     export SSL_CERT_FILE=~/.config/protonmail/bridge/cert.pem
-                    exec ${himalaya}/bin/himalaya
+                    exec nix run github:srid/himalaya/nixify $*
                   '';
               in
               [
