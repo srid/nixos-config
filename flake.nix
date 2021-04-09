14338449b75f11b5061b644f2c23ce3ccda47b04
Sridhar Ratnakumar <srid@srid.ca>
2021-04-09 00:23:51 -0400

    TLP

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
     home-manager.url = "github:nix-community/home-manager";
     himalaya.url = "github:srid/himalaya/nixify-crate2nix";
   };
@@ -11,11 +12,12 @@
     let
       system = "x86_64-linux";
       # Make configuration for any computer I use in my home office.
-      mkHomeMachine = configurationNix: nixpkgs.lib.nixosSystem {
+      mkHomeMachine = configurationNix: thinkpadModule: nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           # System configuration
           configurationNix
+          thinkpadModule
           ./features/passwordstore.nix
           ./features/syncthing.nix
           # ./features/virtualization.nix
@@ -38,7 +40,11 @@
       };
     in
     {
-      nixosConfigurations.p71 = mkHomeMachine ./hosts/p71.nix;
-      nixosConfigurations.x1c7 = mkHomeMachine ./hosts/x1c7.nix;
+      nixosConfigurations.p71 = mkHomeMachine
+        ./hosts/p71.nix
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p53;
+      nixosConfigurations.x1c7 = mkHomeMachine
+        ./hosts/x1c7.nix
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen;
     };
 }
