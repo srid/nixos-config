6e91ff39bb37286771466b8d253c38db20fe5cd6
Sridhar Ratnakumar <srid@srid.ca>
2021-05-08 07:28:16 -0400

    enable lsp

--- config/doom.d/init.el
+++ config/doom.d/init.el
@@ -93,7 +93,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp
+       lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
