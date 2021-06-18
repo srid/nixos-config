7668abb05b04cdf6d2db3843b290c75ab81105ea
Sridhar Ratnakumar <srid@srid.ca>
2021-06-18 19:04:08 -0400

    x1c7: back to xmonad

--- hosts/x1c7.nix
+++ hosts/x1c7.nix
@@ -64,8 +64,8 @@
   programs.mosh.enable = true;
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
 
   users.users.srid = {
     isNormalUser = true;
