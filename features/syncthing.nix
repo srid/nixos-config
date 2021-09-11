dad4ba1817bf9b315b8773880cd7c434717e239c
Sridhar Ratnakumar <srid@srid.ca>
2021-09-11 13:20:44 -0400

    nah

--- features/syncthing.nix
+++ features/syncthing.nix
@@ -4,10 +4,6 @@
       enable = true;
       user = "srid";
       dataDir = "/home/srid";
-      # We want nodes in the same network to be able to connect directly to one
-      # another, without having to go through a third-party relay. So open
-      # firewall ports.
-      openDefaultPorts = true;
     };
   };
 }
