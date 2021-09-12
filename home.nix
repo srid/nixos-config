eea4ff41436410b5f6ec2583b60bc1baf4123193
Sridhar Ratnakumar <srid@srid.ca>
2021-09-12 18:37:54 -0400

    start splitting

--- home.nix
+++ home.nix
@@ -45,30 +45,7 @@ rec {
   ];
 
   programs = {
-    git = {
-      # package = pkgs.gitAndTools.gitFull;
-      enable = true;
-      userName = "Sridhar Ratnakumar";
-      userEmail = "srid@srid.ca";
-      aliases = {
-        co = "checkout";
-        ci = "commit";
-        cia = "commit --amend";
-        s = "status";
-        st = "status";
-        b = "branch";
-        p = "pull --rebase";
-        pu = "push";
-      };
-      ignores = [ "*~" "*.swp" ];
-      extraConfig = {
-        init.defaultBranch = "master";
-        #core.editor = "nvim";
-        #protocol.keybase.allow = "always";
-        credential.helper = "store --file ~/.git-credentials";
-        pull.rebase = "false";
-      };
-    };
+    git = import ./home/git.nix;
 
     tmux = {
       enable = true;
