5f4708b35cc1384665aaed023c7dbf88d58b3442
Sridhar Ratnakumar <srid@srid.ca>
2021-09-30 11:19:27 -0400

    make work in wsl, emanote

--- features/ema/emanote.nix
+++ features/ema/emanote.nix
@@ -3,7 +3,8 @@ let
   emanote = inputs.emanote.outputs.defaultPackage.${system};
 in
 {
-  systemd.user.services.emanote = {
+  # Global service, rather than user service, as the latter doesn't work in NixOS-WSL
+  systemd.services.emanote = {
     description = "Emanote ~/Documents/Notes";
     after = [ "network.target" ];
     wantedBy = [ "default.target" ];
@@ -11,6 +12,7 @@ in
       PORT = "7000";
     };
     serviceConfig = {
+      User = "srid";
       Restart = "always";
       ExecStart = "${emanote}/bin/emanote -L /home/srid/Documents/Notes";
     };
