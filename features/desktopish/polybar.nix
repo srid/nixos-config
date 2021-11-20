abe9ecce5505cc84c7572cd575f55da3f4ba5485
Sridhar Ratnakumar <srid@srid.ca>
2021-11-20 14:56:02 -0500

    add polybar config

--- features/desktopish/polybar.nix
+++ features/desktopish/polybar.nix
@@ -1,14 +1,12 @@
 { config, pkgs, ... }:
-let
-  xmobarPkg = pkgs.callPackage ./xmobar-srid { inherit pkgs; };
-in
+
 {
   systemd.user.services.polybar = {
     enable = true;
     description = "Polybar";
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.polybar}/bin/polybar -c ~/.config/polybar/user example";
+      ExecStart = "${pkgs.polybar}/bin/polybar -c /etc/nixos/config/polybar example";
       Restart = "on-abnormal";
     };
   };
