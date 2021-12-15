eb28936d71d68fb627cbbd299817a7d409a04c51
Sridhar Ratnakumar <srid@srid.ca>
2021-12-15 12:08:15 -0500

    vscode inotify

--- features/desktopish/vscode.nix
+++ features/desktopish/vscode.nix
@@ -13,4 +13,9 @@
     vscode
     nodejs-14_x # Need this for https://nixos.wiki/wiki/Vscode server
   ];
+
+  # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = "524288";
+  };
 }
