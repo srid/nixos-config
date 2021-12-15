40e52a2bd0caeaadcedda4e1be28d6ca8efafb3a
Sridhar Ratnakumar <srid@srid.ca>
2021-12-15 14:17:05 -0500

    plut cache

--- features/caches/platonic.nix
+++ features/caches/platonic.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }: {
   nix.binaryCachePublicKeys = [
     "ci.ardana.platonic.systems:yByqhxfJ9KIUOyiCe3FYhV7GMysJSA3i5JRvgPuySsI="
+    "plutonomicon-pluto.cachix.org-1:ofa/XYzqimDRF96ACKnV7LPiRsz+h4bkHy/a4fbOK04="
   ];
   nix.binaryCaches = [
     "ssh://nix-ssh@ci.ardana.platonic.systems"
+    "https://plutonomicon-pluto.cachix.org"
   ];
 }
