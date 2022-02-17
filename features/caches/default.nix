2f7d7942647e2325961c3e68856219be7df3db3c
Sridhar Ratnakumar <srid@srid.ca>
2022-02-16 19:29:56 -0500

    reflex cache --

--- features/caches/default.nix
+++ features/caches/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   imports = [
     ./iohk.nix
-    ./reflex.nix
+    # ./reflex.nix -- not working on reflex*
     # ./platonic.nix -- don't need ssh cache
     ./cm-idris2-pkgs.nix
   ];
