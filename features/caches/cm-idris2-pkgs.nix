cbd3b4a5262eb92e250e87f1c13a070344a29770
Sridhar Ratnakumar <srid@srid.ca>
2022-02-18 21:21:17 -0500

    Use my cache

--- features/caches/cm-idris2-pkgs.nix
+++ features/caches/cm-idris2-pkgs.nix
@@ -2,8 +2,10 @@
 { pkgs, ... }: {
   nix.binaryCachePublicKeys = [
     "cm-idris2-pkgs.cachix.org-1:YB2oJSEsD5oMJjAESxolC2GQtE6B5I6jkWhte2gtXjk="
+    "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
   ];
   nix.binaryCaches = [
     "https://cm-idris2-pkgs.cachix.org"
+    "https://srid.cachix.org"
   ];
 }
