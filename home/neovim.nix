3f4f0f12eab75cd2047bdc0de6f93b3654e1e044
Sridhar Ratnakumar <srid@srid.ca>
2022-04-09 18:47:34 -0400

    dark better

--- home/neovim.nix
+++ home/neovim.nix
@@ -34,7 +34,7 @@
 
       " papercolor-theme
       " set t_Co=256   " This is may or may not needed.
-      set background=light
+      set background=dark
       colorscheme PaperColor
 
       " telescope
