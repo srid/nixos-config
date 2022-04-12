9226aa69f9ad10bbb86a99022236586a9ebf0d4e
Sridhar Ratnakumar <srid@srid.ca>
2022-04-12 15:34:28 -0400

    tmux color thingy

--- home/tmux.nix
+++ home/tmux.nix
@@ -16,7 +16,12 @@
     ];
 
     extraConfig = ''
-      set -g default-terminal "tmux-256color"
+      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
       # Mouse works as expected
       set-option -g mouse on
       # easy-to-remember split pane commands
