02f6fa9ab89f218b2b40c554be685855a1356226
Sridhar Ratnakumar <srid@srid.ca>
2022-04-16 12:18:33 -0400

    nvim: add keymap for zoxide switch

--- home/neovim.lua
+++ home/neovim.lua
@@ -48,4 +48,6 @@ nmap("<leader>ff", ":Telescope find_files<cr>")
 nmap("<leader>fg", ":Telescope live_grep<cr>")
 nmap("<leader>fb", ":Telescope buffers<cr>")
 nmap("<leader>fh", ":Telescope help_tags<cr>")
+nmap("<leader>fz", ":Telescope zoxide list<cr>")
+
 
