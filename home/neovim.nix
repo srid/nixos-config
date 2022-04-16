92258aadc33b3534b1a0d67b69715607773fb085
Sridhar Ratnakumar <srid@srid.ca>
2022-04-16 19:42:39 -0400

    nvim: enable recommended haskell-vim settings

--- home/neovim.nix
+++ home/neovim.nix
@@ -113,7 +113,20 @@
 
       # Language support
       vim-nix
-      haskell-vim
+      { 
+        plugin = haskell-vim;
+        type = "lua";
+        config = ''
+          vim.cmd([[
+          let g:haskell_enable_quantification = 1   " to enable highlighting of `forall`
+          let g:haskell_enable_recursivedo = 1      " to enable highlighting of `mdo` and `rec`
+          let g:haskell_enable_arrowsyntax = 1      " to enable highlighting of `proc`
+          let g:haskell_enable_pattern_synonyms = 1 " to enable highlighting of `pattern`
+          let g:haskell_enable_typeroles = 1        " to enable highlighting of type roles
+          let g:haskell_enable_static_pointers = 1  " to enable highlighting of `static`
+          ]])
+          '';
+      }
       vim-markdown
     ];
 
