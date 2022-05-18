71ea505c7d6161b22cdbdccbd4b04aea6beed18a
Sridhar Ratnakumar <srid@srid.ca>
2022-05-18 16:01:33 -0400

    Refactor

--- nixos/server/devserver.nix
+++ nixos/server/devserver.nix
@@ -3,19 +3,21 @@
   imports = [
     inputs.nixos-vscode-server.nixosModules.system
   ];
-  environment.systemPackages = with pkgs; [
-    nodejs-16_x # Need this for https://nixos.wiki/wiki/Vscode server
+  environment.systemPackages = [
     # https://old.reddit.com/r/NixOS/comments/uoklud/nix_development_container/i8hn64w/?context=2
-    (pkgs.writeScriptBin "fix-vscode-server" ''
-      #!${pkgs.stdenv.shell}
-      if [[ -d "$HOME/.vscode-server/bin" ]]; then
-        for versiondir in "$HOME"/.vscode-server/bin/*; do
-          echo "!! Fixing $versiondir/node"
-          ln -sf "${pkgs.nodejs-16_x}/bin/node" "$versiondir/node"
-        done
-      fi
-    '')
+    (pkgs.writeShellApplication {
+      name = "fix-vscode-server";
+      text = ''
+        if [[ -d "$HOME/.vscode-server/bin" ]]; then
+          for versiondir in "$HOME"/.vscode-server/bin/*; do
+            echo "!! Fixing $versiondir/node"
+            ln -sf "${pkgs.nodejs-16_x}/bin/node" "$versiondir/node"
+          done
+        fi
+      '';
+    })
   ];
+  # FIXME: This doesn't seem to work; hence the fix-vscode-server above.
   services.auto-fix-vscode-server.enable = true;
 
   # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
