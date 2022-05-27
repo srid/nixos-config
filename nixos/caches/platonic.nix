ba287d3ee94c60133007e00a9800c3d5cfc7a0fd
Sridhar Ratnakumar <srid@srid.ca>
2022-05-27 17:42:17 -0400

    cache: outdated

--- nixos/caches/platonic.nix
+++ nixos/caches/platonic.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }: {
   nix.binaryCachePublicKeys = [
-    "ci.ardana.platonic.systems:yByqhxfJ9KIUOyiCe3FYhV7GMysJSA3i5JRvgPuySsI="
     "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
   ];
   nix.binaryCaches = [
-    "ssh://nix-ssh@ci.ardana.platonic.systems"
     "https://public-plutonomicon.cachix.org"
   ];
 }
