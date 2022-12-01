b18e3b7d8e33c542d728e68085299dd8c1aa004c
Sridhar Ratnakumar <srid@srid.ca>
2022-12-01 17:20:10 -0500

    Switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   inputs = {
     # Principle inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Supportive inputs
-    flake-utils.url = "github:numtide/flake-utils";
     nixos-shell.url = "github:Mic92/nixos-shell";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
@@ -30,127 +30,11 @@
   };
 
   outputs = inputs@{ self, home-manager, nixpkgs, darwin, ... }:
-    let
-      userName = "srid";
-      platformIndependentModules = [
-        ./nixos/caches
-      ];
-      platformIndependentHomeModules = [
-        ./home/tmux.nix
-        ./home/neovim.nix
-        ./home/emacs.nix
-        ./home/starship.nix
-        ./home/terminal.nix
-        ./home/direnv.nix
-      ];
-    in
-    {
-      # Configurations for Linux (NixOS) systems
-      nixosConfigurations =
-        let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-          # Configuration common to all Linux systems
-          commonFeatures = platformIndependentModules ++ [
-            ./nixos/self-ide.nix
-            ./nixos/takemessh
-            ./nixos/current-location.nix
-          ];
-          homeFeatures = [
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit system inputs; };
-              home-manager.users.${userName} = { pkgs, ... }: {
-                imports = platformIndependentHomeModules ++ [
-                  (import ./home/git.nix {
-                    userName = "Sridhar Ratnakumar";
-                    userEmail = "srid@srid.ca";
-                  })
-                  ./home/vscode-server.nix
-                ];
-
-                programs.bash = {
-                  enable = true;
-                } // (import ./home/shellcommon.nix { inherit pkgs; });
-                home.stateVersion = "22.11";
-              };
-              home-manager.users."uday" = {
-                imports = platformIndependentHomeModules ++ [
-                  (import ./home/git.nix {
-                    userName = "Uday Kiran";
-                    userEmail = "udaycruise2903@gmail.com";
-                  })
-                ];
-                programs.bash.enable = true;
-                home.stateVersion = "22.11";
-              };
-            }
-          ];
-          mkLinuxSystem = extraModules: nixpkgs.lib.nixosSystem {
-            inherit system pkgs;
-            # Arguments to pass to all modules.
-            specialArgs = { inherit system inputs; };
-            modules =
-              commonFeatures ++ homeFeatures ++ extraModules;
-          };
-        in
-        {
-          # My Linux development computer (on Hetzner)
-          pinch = mkLinuxSystem
-            [
-              ./systems/hetzner/ax41.nix
-              ./nixos/server/harden.nix
-            ];
-        };
-
-      # Configurations for macOS systems (using nix-darwin)
-      darwinConfigurations =
-        let
-          system = "aarch64-darwin";
-          mkMacosSystem = darwin.lib.darwinSystem;
-          defaultMacosSystem = mkMacosSystem {
-            inherit system;
-            specialArgs = {
-              inherit inputs system;
-              rosettaPkgs = import nixpkgs { system = "x86_64-darwin"; };
-            };
-            modules = platformIndependentModules ++ [
-              ./systems/darwin.nix
-              home-manager.darwinModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = { inherit system inputs; };
-                home-manager.users.${userName} = { pkgs, ... }: {
-                  imports = platformIndependentHomeModules ++ [
-                    (import ./home/git.nix {
-                      userName = "Sridhar Ratnakumar";
-                      userEmail = "srid@srid.ca";
-                    })
-                  ];
-                  programs.zsh = {
-                    enable = true;
-                    initExtra = ''
-                      export PATH=/etc/profiles/per-user/${userName}/bin:/run/current-system/sw/bin/:$PATH
-                    '';
-                  } // (import ./home/shellcommon.nix { inherit pkgs; });
-                  home.stateVersion = "21.11";
-                };
-              }
-            ];
-          };
-        in
-        {
-          default = defaultMacosSystem;
-        };
-    } //
-    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
+    inputs.flake-parts.lib.mkFlake { inherit (inputs) self; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      imports = [ ];
+      perSystem = { self', inputs', config, pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nixpkgs-fmt
             # To enable webhint to analyze source files
@@ -176,13 +60,129 @@
           if system == "aarch64-darwin" then
             bashCmdApp "darwin" ''
               ${self.darwinConfigurations.default.system}/sw/bin/darwin-rebuild \
-                 switch --flake ${self}#default
+                switch --flake ${self}#default
             ''
           else
             bashCmdApp "linux" ''
               nixos-rebuild --use-remote-sudo switch -j auto
             '';
-      }
-    );
+      };
+      flake =
+        let
+          userName = "srid";
+          platformIndependentModules = [
+            ./nixos/caches
+          ];
+          platformIndependentHomeModules = [
+            ./home/tmux.nix
+            ./home/neovim.nix
+            ./home/emacs.nix
+            ./home/starship.nix
+            ./home/terminal.nix
+            ./home/direnv.nix
+          ];
+        in
+        {
+          # Configurations for Linux (NixOS) systems
+          nixosConfigurations =
+            let
+              system = "x86_64-linux";
+              pkgs = nixpkgs.legacyPackages.${system};
+              # Configuration common to all Linux systems
+              commonFeatures = platformIndependentModules ++ [
+                ./nixos/self-ide.nix
+                ./nixos/takemessh
+                ./nixos/current-location.nix
+              ];
+              homeFeatures = [
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.extraSpecialArgs = { inherit system inputs; };
+                  home-manager.users.${userName} = { pkgs, ... }: {
+                    imports = platformIndependentHomeModules ++ [
+                      (import ./home/git.nix {
+                        userName = "Sridhar Ratnakumar";
+                        userEmail = "srid@srid.ca";
+                      })
+                      ./home/vscode-server.nix
+                    ];
 
+                    programs.bash = {
+                      enable = true;
+                    } // (import ./home/shellcommon.nix { inherit pkgs; });
+                    home.stateVersion = "22.11";
+                  };
+                  home-manager.users."uday" = {
+                    imports = platformIndependentHomeModules ++ [
+                      (import ./home/git.nix {
+                        userName = "Uday Kiran";
+                        userEmail = "udaycruise2903@gmail.com";
+                      })
+                    ];
+                    programs.bash.enable = true;
+                    home.stateVersion = "22.11";
+                  };
+                }
+              ];
+              mkLinuxSystem = extraModules: nixpkgs.lib.nixosSystem {
+                inherit system pkgs;
+                # Arguments to pass to all modules.
+                specialArgs = { inherit system inputs; };
+                modules =
+                  commonFeatures ++ homeFeatures ++ extraModules;
+              };
+            in
+            {
+              # My Linux development computer (on Hetzner)
+              pinch = mkLinuxSystem
+                [
+                  ./systems/hetzner/ax41.nix
+                  ./nixos/server/harden.nix
+                ];
+            };
+
+          # Configurations for macOS systems (using nix-darwin)
+          darwinConfigurations =
+            let
+              system = "aarch64-darwin";
+              mkMacosSystem = darwin.lib.darwinSystem;
+              defaultMacosSystem = mkMacosSystem {
+                inherit system;
+                specialArgs = {
+                  inherit inputs system;
+                  rosettaPkgs = import nixpkgs { system = "x86_64-darwin"; };
+                };
+                modules = platformIndependentModules ++ [
+                  ./systems/darwin.nix
+                  home-manager.darwinModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.extraSpecialArgs = { inherit system inputs; };
+                    home-manager.users.${userName} = { pkgs, ... }: {
+                      imports = platformIndependentHomeModules ++ [
+                        (import ./home/git.nix {
+                          userName = "Sridhar Ratnakumar";
+                          userEmail = "srid@srid.ca";
+                        })
+                      ];
+                      programs.zsh = {
+                        enable = true;
+                        initExtra = ''
+                          export PATH=/etc/profiles/per-user/${userName}/bin:/run/current-system/sw/bin/:$PATH
+                        '';
+                      } // (import ./home/shellcommon.nix { inherit pkgs; });
+                      home.stateVersion = "21.11";
+                    };
+                  }
+                ];
+              };
+            in
+            {
+              default = defaultMacosSystem;
+            };
+        };
+    };
 }
