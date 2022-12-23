cb121622df34ce2595bb74c02c6884053889b0c2
Sridhar Ratnakumar <srid@srid.ca>
2022-12-23 15:01:13 -0500

    garnix -> my cache

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }: {
   nix.settings.trusted-public-keys = [
-    # "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+    "cache.srid.ca:8sQkbPrOIoXktIwI0OucQBXod2e9fDjjoEZWn8OXbdo="
     # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
   nix.settings.substituters = [
-    # "https://srid.cachix.org"
+    "https://cache.srid.ca"
     # "https://nix-community.cachix.org"
-    "https://cache.garnix.io"
+    # "https://cache.garnix.io"
   ];
 }
