8d1c86f156ae4bf1a04787126ae46caa817e573f
Sridhar Ratnakumar <srid@srid.ca>
2022-12-27 14:50:50 -0500

    rekey instruction

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,6 +5,8 @@ let
       (import ../systems/hetzner/ax41.info.nix).hostKeyPub
     ];
 in
+# How I rekey on macOS:
+# agenix  -r -i =(op read 'op://Personal/id_rsa/private key')
 {
   "cache-priv-key.age".publicKeys = keys;
 }
