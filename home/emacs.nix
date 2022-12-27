6c11401a38c3aa051733aa207ca4535217f62d8f
Sridhar Ratnakumar <srid@srid.ca>
2022-12-26 22:33:21 -0500

    emacs: compile with vterm

--- home/emacs.nix
+++ home/emacs.nix
@@ -3,27 +3,19 @@
 {
   programs.emacs = {
     enable = true;
-    package = (inputs.emacs-overlay.packages.${system}.emacsPgtk.override {
-      withXwidgets = true;
-    }).overrideAttrs (oa: {
-      buildInputs = oa.buildInputs ++ lib.optionals pkgs.stdenv.isDarwin
-        [ pkgs.darwin.apple_sdk.frameworks.WebKit ];
-    });
+    package =
+      let
+        emacsPgtkWithXwidgets = inputs.emacs-overlay.packages.${system}.emacsPgtk.override {
+          withXwidgets = true;
+        };
+        myEmacs = emacsPgtkWithXwidgets.overrideAttrs (oa: {
+          buildInputs = oa.buildInputs ++ lib.optionals pkgs.stdenv.isDarwin
+            [ pkgs.darwin.apple_sdk.frameworks.WebKit ];
+        });
+      in
+      myEmacs.passthru.pkgs.emacsWithPackages (epkgs: [
+        epkgs.vterm
+      ]);
   };
 
-  # https://docs.doomemacs.org/latest/modules/term/vterm/
-  #
-  # Sadly, this doesn't work yet. This is the blocker:
-  # https://github.com/akermu/emacs-libvterm/issues/333#issuecomment-1272195411
-  home.packages = with pkgs; [
-    cmake
-    libvterm-neovim
-    (pkgs.writeShellApplication {
-      name = "glibtool";
-      runtimeInputs = [ libtool ];
-      text = ''
-        libtool "$@"
-      '';
-    })
-  ];
 }
