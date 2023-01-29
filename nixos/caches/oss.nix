19bf0c2080e31c2df2a07f3ab724c0375cc8cf7a
Sridhar Ratnakumar <srid@srid.ca>
2023-01-29 12:58:14 -0500

    do not use my cache either

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   nix.settings.trusted-public-keys = [
-    (builtins.readFile ../../secrets/cache-pub-key)
+    # (builtins.readFile ../../secrets/cache-pub-key)
     # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
   nix.settings.substituters = [
-    "https://cache.srid.ca"
+    # "https://cache.srid.ca"
     # "https://nix-community.cachix.org"
     # "https://cache.garnix.io"
   ];
