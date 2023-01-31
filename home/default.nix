d1f7f31400cf9c299949d4b25a5a2fd8d217b353
Sridhar Ratnakumar <srid@srid.ca>
2023-01-31 11:32:19 -0500

    darwin: set ssh auth sock for 1p

--- home/default.nix
+++ home/default.nix
@@ -34,6 +34,9 @@
           export PATH=/etc/profiles/per-user/$USER/bin:/run/current-system/sw/bin/:$PATH
           # For 1Password CLI. This requires `pkgs.gh` to be installed.
           source $HOME/.config/op/plugins.sh
+          # Because, adding it in .ssh/config is not enough.
+          # cf. https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
+          export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
         '';
       };
     };
