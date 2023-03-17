afea97cb06bb3ba8be758930644c147f38bbf605
Sridhar Ratnakumar <srid@srid.ca>
2023-03-17 16:55:42 -0400

    commemnts

--- nixos/jenkins.nix
+++ nixos/jenkins.nix
@@ -2,25 +2,33 @@
 
 # HOWTO:
 # - Goto http://localhost:8080/
+#
+# TODO:
+# - JCasC Nixified for configuration
+# - Build agents (SSH slave)
+#    - macOS slave
+# - Github integration
+#   https://github.com/jenkinsci/github-branch-source-plugin/blob/master/docs/github-app.adoc#configuration-as-code-plugin
 let
   port = 9091;
 in
 {
   services.jenkins = {
     enable = true;
+    inherit port;
     packages = with pkgs; [
       # Add packages used by Jenkins plugins here.
       nix
       git
       bash # 'sh' step requires this
     ];
-    inherit port;
     # ./jenkins/update-plugins.sh
     plugins = import ./jenkins/plugins.nix {
       inherit (pkgs) fetchurl stdenv;
     };
     extraJavaOptions = [
       # Useful when the 'sh' step b0rks.
+      # https://stackoverflow.com/a/66098536/55246
       "-Dorg.jenkinsci.plugins.durabletask.BourneShellScript.LAUNCH_DIAGNOSTICS=true"
     ];
   };
