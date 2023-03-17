b536472e98829e8b49a1a200295f8ccc79c48536
Sridhar Ratnakumar <srid@srid.ca>
2023-03-17 16:57:01 -0400

    fix plugin updater

--- nixos/jenkins/update-plugins.sh
+++ nixos/jenkins/update-plugins.sh
@@ -2,7 +2,7 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PLUGINS_NIX="${SCRIPT_DIR}/plugins.nix"
-echo "Updating ${PLUGINS_NIX}
+echo "Updating ${PLUGINS_NIX}"
 set -x
 nix run github:Fuuzetsu/jenkinsPlugins2nix -- \
     -p github-api \
