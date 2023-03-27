6a8141ad5ead2fcc9b55c5c3e9e87f140192b963
Sridhar Ratnakumar <srid@srid.ca>
2023-03-27 13:02:26 -0400

    add cache-server priv key

--- nixos/cache-server.nix
+++ nixos/cache-server.nix
@@ -7,25 +7,25 @@
   ];
 
   # Cache server
-  age.secrets.${keyName}.file = ../secrets/${keyName}.age;
+  sops.secrets."cache-server/private-key".owner = "root";
   services.nix-serve = {
     enable = true;
-    secretKeyFile = config.age.secrets.${keyName}.path;
-  };
-  nix.settings.allowed-users = [ "nix-serve" ];
-  nix.settings.trusted-users = [ "nix-serve" ];
+    secretKeyFile = config.sops.secrets."cache-server/private-key".path
+      };
+    nix.settings.allowed-users = [ "nix-serve" ];
+    nix.settings.trusted-users = [ "nix-serve" ];
 
-  # Web servr
-  services.nginx = {
-    virtualHosts.${domain} = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/".extraConfig = ''
-        proxy_pass http://localhost:${toString config.services.nix-serve.port};
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      '';
+    # Web servr
+    services.nginx = {
+      virtualHosts.${domain} = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/".extraConfig = ''
+          proxy_pass http://localhost:${toString config.services.nix-serve.port};
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        '';
+      };
     };
-  };
-}
+  }
