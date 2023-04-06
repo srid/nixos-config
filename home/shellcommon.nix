e45c366a162b4008eb51440deb1e60c9a5271319
Sridhar Ratnakumar <srid@srid.ca>
2023-04-06 14:27:15 -0400

    Better way to open neovim with fzf

--- home/shellcommon.nix
+++ home/shellcommon.nix
@@ -3,12 +3,14 @@ let
   # These aliases should work on all shells.
   shellAliasesSimple = {
     e = "nvim";
+    ee = ''
+      fzf --bind "enter:execute(nvim {})"
+    '';
     g = "${pkgs.git}/bin/git";
     lg = "lazygit";
   };
   # These aliases should work on bash/zsh.
   shellAliases = shellAliasesSimple // {
-    ee = "nvim \"$(fzf)\"";
     ls = "${pkgs.exa}/bin/exa";
     l = "ls";
     ll = "ls -l";
@@ -21,5 +23,6 @@ in
   programs.bash = { inherit shellAliases; };
   programs.zsh = { inherit shellAliases; };
   programs.nushell.extraConfig =
-    lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "alias ${k} = ${v}") shellAliasesSimple);
+    lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "alias ${k} = ${v} ") shellAliasesSimple);
 }
+
