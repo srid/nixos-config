5521eb2e64a14353103b2e06eaaf5f36d23dc71a
Sridhar Ratnakumar <srid@srid.ca>
2023-04-07 15:27:31 -0400

    add docker to path

--- home/default.nix
+++ home/default.nix
@@ -13,8 +13,9 @@
         ];
         programs.nushell = {
           enable = true;
+          # TODO: Refactor using lists; or even upstream PATH setting to home-manager
           envFile.text = ''
-            let-env PATH = ($env.PATH | split row (char esep) | prepend $"/etc/profiles/per-user/($env.USER)/bin" | prepend '/run/current-system/sw/bin/')
+            let-env PATH = ( $env.PATH | split row (char esep) | prepend $"/etc/profiles/per-user/($env.USER)/bin" | prepend '/run/current-system/sw/bin/' | prepend '/Applications/Docker.app/Contents/Resources/bin/')
           '';
         };
       };
