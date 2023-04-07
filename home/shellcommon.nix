035a337eb1e066c622062ff55e24d82dfb7d9c11
Sridhar Ratnakumar <srid@srid.ca>
2023-04-07 15:27:12 -0400

    comment

--- home/shellcommon.nix
+++ home/shellcommon.nix
@@ -12,6 +12,7 @@ in
 {
   programs.bash = { inherit shellAliases; };
   programs.zsh = { inherit shellAliases; };
+  # Until https://github.com/nix-community/home-manager/pull/3529
   programs.nushell.extraConfig =
     lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "alias ${k} = ${v} ") shellAliases);
 }
