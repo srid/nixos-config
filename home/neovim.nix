842baf4520c0abf71787dac07470be574a2b8c47
Sridhar Ratnakumar <srid@srid.ca>
2023-04-07 15:40:30 -0400

    Add nvim-nu

--- home/neovim.nix
+++ home/neovim.nix
@@ -71,6 +71,24 @@
       # Language support
       vim-nix
 
+      # nushell
+      null-ls-nvim
+      { 
+        plugin = nvim-nu;
+        type = "lua";
+        config = ''
+          require'nu'.setup{
+            use_lsp_features = true, -- requires https://github.com/jose-elias-alvarez/null-ls.nvim
+            -- lsp_feature: all_cmd_names is the source for the cmd name completion.
+            -- It can be
+            --  * a string, which is interpreted as a shell command and the returned list is the source for completions (requires plenary.nvim)
+            --  * a list, which is the direct source for completions (e.G. all_cmd_names = {"echo", "to csv", ...})
+            --  * a function, returning a list of strings and the return value is used as the source for completions
+            all_cmd_names = [[nu -c 'help commands | get name | str join "\n"']]
+        }
+        '';
+      }
+
       vim-markdown
     ];
 
