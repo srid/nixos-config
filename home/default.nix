2eab1a8f218c982a70c7a14b53fa22a32a90f6b4
Sridhar Ratnakumar <srid@srid.ca>
2023-04-07 15:27:40 -0400

    nushell: use vi mode

--- home/default.nix
+++ home/default.nix
@@ -17,6 +17,11 @@
           envFile.text = ''
             let-env PATH = ( $env.PATH | split row (char esep) | prepend $"/etc/profiles/per-user/($env.USER)/bin" | prepend '/run/current-system/sw/bin/' | prepend '/Applications/Docker.app/Contents/Resources/bin/')
           '';
+          configFile.text = ''
+            let-env config = {
+              edit_mode: vi
+            }
+          '';
         };
       };
       common-linux = {
