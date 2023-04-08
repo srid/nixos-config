e964ade9bb1e12280c68401054beb4cafdc0e6fe
Sridhar Ratnakumar <srid@srid.ca>
2023-04-08 11:26:11 -0400

    Improve starship config

--- home/starship.nix
+++ home/starship.nix
@@ -3,12 +3,15 @@
     enable = true;
     settings = {
       username = {
-        format = "[$user](bold blue) ";
+        style_user = "blue bold";
+        style_root = "red bold";
+        format = "[$user]($style) ";
         disabled = false;
         show_always = true;
       };
       hostname = {
         ssh_only = false;
+        ssh_symbol = "🌐 ";
         format = "on [$hostname](bold red) ";
         trim_at = ".local";
         disabled = false;
