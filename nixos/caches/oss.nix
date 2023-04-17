63e7e3239efa91c8cba35277a1555daa8122873d
Sridhar Ratnakumar <srid@srid.ca>
2023-04-17 13:12:45 -0400

    add nammayatri cache

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -3,10 +3,12 @@
     # (builtins.readFile ../../secrets/cache-pub-key)
     # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
   ];
   nix.settings.substituters = [
     # "https://cache.srid.ca"
     # "https://nix-community.cachix.org"
     # "https://cache.garnix.io"
+    "https://nammayatri.cachix.org"
   ];
 }
