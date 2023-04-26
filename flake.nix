9148cf60bfdba8f3f8143e1682d227b1fe7bfcb9
Sridhar Ratnakumar <srid@srid.ca>
2023-04-25 20:00:22 -0400

    mv to new file

--- flake.nix
+++ flake.nix
@@ -90,82 +90,10 @@
             ];
             networking.defaultGateway = "167.235.115.129";
             networking.nameservers = [ "8.8.8.8" ];
-            disko.devices =
-              let disks = [ "/dev/nvme0n1" ]; # "/dev/nvme1n1" ];
-              in {
-                disk = inputs.nixpkgs.lib.genAttrs disks (dev: {
-                  device = dev;
-                  type = "disk";
-                  content = {
-                    type = "table";
-                    format = "gpt";
-                    partitions = [
-                      {
-                        name = "boot";
-                        # type = "partition";
-                        start = "0";
-                        end = "1M";
-                        part-type = "primary";
-                        flags = [ "bios_grub" ];
-                      }
-                      {
-                        # type = "partition";
-                        name = "ESP";
-                        start = "1MiB";
-                        end = "100MiB";
-                        bootable = true;
-                        content = {
-                          type = "mdraid";
-                          name = "boot";
-                        };
-                      }
-                      {
-                        name = "root";
-                        # type = "partition";
-                        start = "100MiB";
-                        end = "100%";
-                        part-type = "primary";
-                        bootable = true;
-                        content = {
-                          type = "lvm_pv";
-                          vg = "pool";
-                        };
-                      }
-                    ];
-                  };
-                });
-                mdadm = {
-                  boot = {
-                    type = "mdadm";
-                    level = 1;
-                    metadata = "1.0";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                    };
-                  };
-                };
-                lvm_vg = {
-                  pool = {
-                    type = "lvm_vg";
-                    lvs = {
-                      root = {
-                        # type = "lvm_lv";
-                        size = "100%FREE";
-                        content = {
-                          type = "filesystem";
-                          format = "ext4";
-                          mountpoint = "/";
-                          mountOptions = [
-                            "defaults"
-                          ];
-                        };
-                      };
-                    };
-                  };
-                };
-              };
+            disko.devices = import ./disko/working.nix {
+              lib = inputs.nixpkgs.lib;
+              disks = [ "/dev/nvme0n1" ]; # "/dev/nvme1n1" ];
+            };
           });
           pce = self.nixos-flake.lib.mkLinuxSystem {
             imports = [
