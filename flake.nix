b3ed50dae577d21a7c2601854f39700329b7227a
Sridhar Ratnakumar <srid@srid.ca>
2023-04-25 20:17:22 -0400

    separate file

--- flake.nix
+++ flake.nix
@@ -56,45 +56,12 @@
       flake = {
         # Configurations for Linux (NixOS) systems
         nixosConfigurations = {
-          actual = self.nixos-flake.lib.mkLinuxSystem ({ modulesPath, ... }: {
+          actual = self.nixos-flake.lib.mkLinuxSystem {
             imports = [
-              (modulesPath + "/installer/scan/not-detected.nix")
               self.nixosModules.default # Defined in nixos/default.nix
-              inputs.disko.nixosModules.disko
+              ./systems/hetzner/ex101.nix
             ];
-            system.stateVersion = "22.11";
-            services.openssh.enable = true;
-            boot.loader.grub = {
-              devices = [ "/dev/nvme0n1" "/dev/nvme1n1" ];
-              efiSupport = true;
-              efiInstallAsRemovable = true;
-            };
-            boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
-            nixpkgs.hostPlatform = "x86_64-linux";
-            # powerManagement.cpuFreqGovernor = "ondemand";
-            hardware.cpu.intel.updateMicrocode = true;
-            hardware.enableRedistributableFirmware = true;
-
-            networking.hostName = "actual";
-            networking.useDHCP = false;
-            networking.interfaces."eth0".ipv4.addresses = [
-              {
-                address = "167.235.115.189"; # your IPv4 here
-                prefixLength = 24;
-              }
-            ];
-            networking.interfaces."eth0".ipv6.addresses = [
-              {
-                address = "2a01:4f8:2200:17c6::1"; # Your IPv6 here
-                prefixLength = 64;
-              }
-            ];
-            networking.defaultGateway = "167.235.115.129";
-            networking.nameservers = [ "8.8.8.8" ];
-            disko.devices = import ./disko/two-raids-on-two-disks.nix {
-              lib = inputs.nixpkgs.lib;
-            };
-          });
+          };
           pce = self.nixos-flake.lib.mkLinuxSystem {
             imports = [
               self.nixosModules.default # Defined in nixos/default.nix
