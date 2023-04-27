cd5cffadf3879cc4c3b63d7aff35f8ab241ba188
Sridhar Ratnakumar <srid@srid.ca>
2023-04-27 09:34:48 -0400

    nix: conditional on darwin

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,19 +1,24 @@
-{ flake, ... }:
+{ flake, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.allowBroken = true;
-  nixpkgs.config.allowUnsupportedSystem = true;
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [
-    flake.inputs.nuenv.overlays.nuenv
-    (self: super: { devour-flake = self.callPackage flake.inputs.devour-flake { }; })
-  ];
+  nixpkgs = {
+    config = {
+      allowBroken = true;
+      allowUnsupportedSystem = true;
+      allowUnfree = true;
+    };
+    overlays = [
+      flake.inputs.nuenv.overlays.nuenv
+      (self: super: { devour-flake = self.callPackage flake.inputs.devour-flake { }; })
+    ];
+  };
 
   nix = {
     nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
-    settings.extra-platforms = "aarch64-darwin x86_64-darwin";
     settings.experimental-features = "nix-command flakes repl-flake";
+    # I don't have an Intel mac.
+    settings.extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
   };
 }
 
