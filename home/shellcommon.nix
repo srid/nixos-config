e1bf6938b668e3c2b6466e50ca19109c5b99661d
Sridhar Ratnakumar <srid@srid.ca>
2023-04-27 09:08:28 -0400

    Use home.shellAliases

--- home/shellcommon.nix
+++ home/shellcommon.nix
@@ -1,17 +1,15 @@
 { pkgs, lib, ... }:
-let
-  shellAliases = {
+
+{
+  home.shellAliases = {
     e = "nvim";
     g = "git";
     lg = "lazygit";
     l = lib.getExe pkgs.exa;
   };
-in
-{
-  programs.bash = { inherit shellAliases; };
-  programs.zsh = { inherit shellAliases; };
+
   # Until https://github.com/nix-community/home-manager/pull/3529
-  programs.nushell.extraConfig =
-    lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "alias ${k} = ${v} ") shellAliases);
+  # programs.nushell.extraConfig =
+  #  lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "alias ${k} = ${v} ") config.home.shellAliases);
 }
 
