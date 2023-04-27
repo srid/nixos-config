411274ba9a2452cbb40926e4ee74cc08d40830e3
Sridhar Ratnakumar <srid@srid.ca>
2023-04-27 09:48:39 -0400

    Use empty flake registry; resolves #18

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -16,9 +16,13 @@
   nix = {
     nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
-    settings.experimental-features = "nix-command flakes repl-flake";
-    # I don't have an Intel mac.
-    settings.extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+    settings = {
+      experimental-features = "nix-command flakes repl-flake";
+      # I don't have an Intel mac.
+      extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+      # Nullify the registry for purity.
+      flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
+    };
   };
 }
 
