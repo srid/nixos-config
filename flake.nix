38967e7489daf8e5092ec88d051ec55ab8e1bb72
Sridhar Ratnakumar <srid@srid.ca>
2023-05-15 12:26:47 -0400

    Update primary inputs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Principle inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-darwin.url = "github:lnl7/nix-darwin/master";
+    nix-darwin.url = "github:lnl7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
