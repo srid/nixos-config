eb103395db351532e844a061a7747fec7f638d5f
Sridhar Ratnakumar <srid@srid.ca>
2023-07-15 11:31:02 -0400

    Fix a syntax error

--- nixos/cache-server.nix
+++ nixos/cache-server.nix
@@ -10,22 +10,22 @@
   sops.secrets."cache-server/private-key".owner = "root";
   services.nix-serve = {
     enable = true;
-    secretKeyFile = config.sops.secrets."cache-server/private-key".path
-      };
-    nix.settings.allowed-users = [ "nix-serve" ];
-    nix.settings.trusted-users = [ "nix-serve" ];
+    secretKeyFile = config.sops.secrets."cache-server/private-key".path;
+  };
+  nix.settings.allowed-users = [ "nix-serve" ];
+  nix.settings.trusted-users = [ "nix-serve" ];
 
-    # Web servr
-    services.nginx = {
-      virtualHosts.${domain} = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/".extraConfig = ''
-          proxy_pass http://localhost:${toString config.services.nix-serve.port};
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        '';
-      };
+  # Web servr
+  services.nginx = {
+    virtualHosts.${domain} = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/".extraConfig = ''
+        proxy_pass http://localhost:${toString config.services.nix-serve.port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
     };
-  }
+  };
+}
