153930f2c0b824266a2528cb65fb6d9ce1733c7d
Sridhar Ratnakumar <srid@srid.ca>
2023-07-22 11:27:14 -0400

    Update flake-parts (not crashing this time; #31)

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
   };
 
   outputs = inputs@{ self, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit (inputs) self; } {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       imports = [
         inputs.treefmt-nix.flakeModule
