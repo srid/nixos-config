e27bf824cce0dada95fd27b5e75d000a779b35fe
Sridhar Ratnakumar <srid@srid.ca>
2023-08-25 11:16:46 -0400

    ny cache

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -1,12 +1,12 @@
 {
   nix.settings.trusted-public-keys = [
     #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    #"nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
+    "nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
   nix.settings.substituters = [
     #"https://nix-community.cachix.org"
-    #"https://nammayatri.cachix.org"
+    "https://nammayatri.cachix.org"
     "https://cache.garnix.io"
   ];
 }
