85cc8bd3b6adb70eac549219cd3f69f6ce8100bc
Sridhar Ratnakumar <srid@srid.ca>
2024-01-17 19:32:04 +0530

    Make emacs.nix work

--- home/emacs.nix
+++ home/emacs.nix
@@ -2,32 +2,13 @@
 # Or:
 #
 #   open -a ~/Applications/Home\ Manager\ Apps/Emacs.app
-{ pkgs, lib, flake, ... }:
+{ pkgs, flake, ... }:
 
-let
-  package =
-    let
-      # Pgtk won't be available until emacs 29; so we must use the nightly overlay
-      # cf. https://github.com/NixOS/nixpkgs/issues/192692#issuecomment-1256872679
-      emacsPgtkWithXwidgets = flake.inputs.emacs-overlay.packages.${pkgs.system}.emacsPgtk.override {
-        withXwidgets = true;
-      };
-      myEmacs = emacsPgtkWithXwidgets.overrideAttrs (oa: {
-        buildInputs = oa.buildInputs ++ lib.optionals pkgs.stdenv.isDarwin
-          [ pkgs.darwin.apple_sdk.frameworks.WebKit ];
-      });
-    in
-    (pkgs.emacsPackagesFor myEmacs).emacsWithPackages (epkgs: [
-      epkgs.vterm
-    ]);
-in
 {
   imports = [ flake.inputs.nix-doom-emacs.hmModule ];
 
   programs.doom-emacs = {
     enable = true;
-    # Disabling until https://github.com/nix-community/nix-doom-emacs/issues/409
-    # emacsPackage = package;
     emacsPackage = pkgs.emacs29-pgtk;
     doomPrivateDir = ./emacs/doom;
   };
