cecbdb9ffbc8b358208490415df593e0be2ca191
Sridhar Ratnakumar <srid@srid.ca>
2024-01-18 23:22:34 +0530

    fortune cookies

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.zsh = {
@@ -14,6 +14,10 @@
       # Because, adding it in .ssh/config is not enough.
       # cf. https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
       export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    '';
+
+    initExtra = ''
+      ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.charasay} say -r
     '';
   };
 
