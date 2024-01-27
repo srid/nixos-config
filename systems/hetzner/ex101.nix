a445d52819a44c0390112f339fd3861a0d4fd083
Sridhar Ratnakumar <srid@srid.ca>
2024-01-27 05:34:27 -0500

    fmt

--- systems/hetzner/ex101.nix
+++ systems/hetzner/ex101.nix
@@ -5,34 +5,39 @@
   ];
   system.stateVersion = "22.11";
   services.openssh.enable = true;
-  boot.loader.grub = {
-    devices = [ "/dev/nvme0n1" "/dev/nvme1n1" ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+  boot = {
+    loader.grub = {
+      devices = [ "/dev/nvme0n1" "/dev/nvme1n1" ];
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
+    binfmt.emulatedSystems = [ "aarch64-linux" ]; # For cross-compiling, https://discourse.nixos.org/t/how-do-i-cross-compile-a-flake/12062/4?u=srid
   };
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ]; # For cross-compiling, https://discourse.nixos.org/t/how-do-i-cross-compile-a-flake/12062/4?u=srid
   nixpkgs.hostPlatform = "x86_64-linux";
-  # powerManagement.cpuFreqGovernor = "ondemand";
-  hardware.cpu.intel.updateMicrocode = true;
-  hardware.enableRedistributableFirmware = true;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableRedistributableFirmware = true;
+  };
 
-  networking.hostName = "actual";
-  networking.useDHCP = false;
-  networking.interfaces."eth0".ipv4.addresses = [
-    {
-      address = "167.235.115.189"; # your IPv4 here
-      prefixLength = 24;
-    }
-  ];
-  networking.interfaces."eth0".ipv6.addresses = [
-    {
-      address = "2a01:4f8:2200:17c6::1"; # Your IPv6 here
-      prefixLength = 64;
-    }
-  ];
-  networking.defaultGateway = "167.235.115.129";
-  networking.nameservers = [ "8.8.8.8" ];
+  networking = {
+    hostName = "actual";
+    useDHCP = false;
+    interfaces."eth0".ipv4.addresses = [
+      {
+        address = "167.235.115.189"; # your IPv4 here
+        prefixLength = 24;
+      }
+    ];
+    interfaces."eth0".ipv6.addresses = [
+      {
+        address = "2a01:4f8:2200:17c6::1"; # Your IPv6 here
+        prefixLength = 64;
+      }
+    ];
+    defaultGateway = "167.235.115.129";
+    nameservers = [ "8.8.8.8" ];
+  };
   disko.devices = import ./disko/two-raids-on-two-disks.nix {
     inherit lib;
   };
