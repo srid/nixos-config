5c465c34bdc57c2b6d2b3d115e6d852f7eca891d
Sridhar Ratnakumar <srid@srid.ca>
2024-01-27 01:09:32 -0500

    options

--- nixos/github-runner.nix
+++ nixos/github-runner.nix
@@ -1,28 +1,62 @@
 { pkgs, lib, config, ... }:
 let
+  inherit (lib) types;
   getRunnerUser = name:
     # systemd DynamicUser
     "github-runner-${name}";
 in
 {
-  sops.secrets."gh-selfhosted-tokens/emanote".mode = "0440";
-
-  # TODO: Run inside container
-  services.github-runners = {
-    emanote = {
-      enable = true;
-      name = "emanote";
-      tokenFile = config.sops.secrets."gh-selfhosted-tokens/emanote".path;
-      url = "https://github.com/srid/emanote";
-      extraPackages = [ pkgs.cachix pkgs.nixci ];
-      extraLabels = [ "nixos" ];
+  options = {
+    services.personal-github-runners = lib.mkOption {
+      default = { };
+      type = types.submodule {
+        owner = lib.mkOption {
+          type = types.str;
+          default = "srid";
+        };
+        repositories = lib.mkOption {
+          type = types.listOf types.str;
+          default = [
+            # My repositories configured to use self-hosted runners
+            "emanote"
+          ];
+        };
+        sopsPrefix = lib.mkOption {
+          type = types.str;
+          default = "gh-selfhosted-tokens";
+        };
+        runnerConfig = lib.mkOption {
+          type = types.deferredModule;
+          default = {
+            extraPackages = [ pkgs.cachix pkgs.nixci ];
+            extraLabels = [ "nixos" ];
+          };
+        };
+      };
     };
   };
-  nix.settings.trusted-users =
-    lib.mapAttrsToList
-      (name: runner:
-        if runner.user == null
-        then getRunnerUser name
-        else runner.user)
-      config.services.github-runners;
+  config = {
+    sops.secrets = {
+      "gh-selfhosted-tokens/emanote".mode = "0440";
+    };
+
+    # TODO: Run inside container
+    services.github-runners = {
+      emanote = {
+        enable = true;
+        name = "emanote";
+        tokenFile = config.sops.secrets."gh-selfhosted-tokens/emanote".path;
+        url = "https://github.com/srid/emanote";
+        extraPackages = [ pkgs.cachix pkgs.nixci ];
+        extraLabels = [ "nixos" ];
+      };
+    };
+    nix.settings.trusted-users =
+      lib.mapAttrsToList
+        (name: runner:
+          if runner.user == null
+          then getRunnerUser name
+          else runner.user)
+        config.services.github-runners;
+  };
 }
