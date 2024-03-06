690b461868142033cc7c9dc012aee8c3bf4ca8b8
Sridhar Ratnakumar <srid@srid.ca>
2024-03-06 08:54:12 -0500

    fuckport: add desc

--- packages/fuckport.nix
+++ packages/fuckport.nix
@@ -1,11 +1,14 @@
 { writeShellApplication, jc, jq, ... }:
 
-# Kill the process with the port open
-# Used only to kill stale ghc.
-# FIXME: This doesn't work when lsof returns *multiple* processes.
 writeShellApplication {
   name = "fuckport";
   runtimeInputs = [ jc jq ];
+  meta.description = ''
+    Kill the process with the port open
+
+    Used only to kill stale ghc.
+  '';
+  # FIXME: This doesn't work when lsof returns *multiple* processes.
   text = ''
     lsof -i :"$1"
     THEPID=$(lsof -i :"$1" | jc --lsof 2> /dev/null | jq '.[].pid')
