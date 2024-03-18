94f56d817d2b8db400d483f4862d78e3d8aef5fd
Sridhar Ratnakumar <srid@srid.ca>
2024-03-18 08:54:09 -0400

    helix: start using it

--- home/helix.nix
+++ home/helix.nix
@@ -1,5 +1,12 @@
 {
   programs.helix = {
     enable = true;
+    settings = {
+      theme = "snazzy";
+      editor .true-color = true;
+      keys.insert.j.j = "normal_mode";
+      keys.insert.s.s = ":write"; # TODO: this should also put back in normal_mode
+      keys.normal.s.s = ":write";
+    };
   };
 }
