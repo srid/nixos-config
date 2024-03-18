ee0eac6b07e2be5b48ad1b602e0211e004e9d89c
Sridhar Ratnakumar <srid@srid.ca>
2024-03-18 09:09:35 -0400

    helix: C-s to save

--- home/helix.nix
+++ home/helix.nix
@@ -5,8 +5,9 @@
       theme = "snazzy";
       editor .true-color = true;
       keys.insert.j.j = "normal_mode";
-      keys.insert.s.s = ":write"; # TODO: this should also put back in normal_mode
-      keys.normal.s.s = ":write";
+      # Shortcut to save file, in any mode.
+      keys.insert."C-s" = [":write" "normal_mode"];
+      keys.normal."C-s" = ":write";
     };
   };
 }
