41e3faec87d7adb0a07df38797bf19c24be02b8a
Sridhar Ratnakumar <srid@srid.ca>
2024-03-26 14:32:43 -0400

    Typo

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
 
   outputs = inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarcch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.nixos-flake.flakeModule
