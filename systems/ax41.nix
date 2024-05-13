849a4dc51708deb3fc9039b81526a15998f0c4f2
Sridhar Ratnakumar <srid@srid.ca>
2024-05-13 14:11:35 +1000

    add actualism-app server

--- systems/ax41.nix
+++ systems/ax41.nix
@@ -46,4 +46,38 @@ in
   services.openssh.enable = true;
 
   programs.nix-ld.enable = true; # for vscode server
+
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+  # actualism-app (temp host)
+  services.nginx = {
+    enable = true;
+    virtualHosts."www.actualism.app" = {
+      # enableACME = true;
+      # forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8080";
+        proxyWebsockets = true;
+      };
+    };
+  };
+  security.acme.certs = {
+    "www.actualism.app".email = "srid@srid.ca";
+  };
+  systemd.services.actualism-app = {
+    enable = true;
+    description = "actualism-app server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart =
+        lib.getExe (pkgs.writeShellApplication {
+          name = "actualism-app-start";
+          text = ''
+            cd ${pkgs.actualism-app}/ 
+            ${pkgs.actualism-app}/bin/actualism-app
+          '';
+        });
+      Restart = "always";
+    };
+  };
 }
