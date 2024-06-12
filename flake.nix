76d27ecf8ea217b959ab5bc37845a22209cdade9
Sridhar Ratnakumar <srid@srid.ca>
2024-06-12 00:07:19 -0400

    refactor

--- flake.nix
+++ flake.nix
@@ -101,14 +101,16 @@
       perSystem = { self', pkgs, system, config, ... }: {
         # Flake inputs we want to update periodically
         # Run: `nix run .#update`.
-        nixos-flake.primary-inputs = [
-          "nixpkgs"
-          "home-manager"
-          "nix-darwin"
-          "nixos-flake"
-          "nix-index-database"
-          "nixvim"
-        ];
+        nixos-flake = {
+          primary-inputs = [
+            "nixpkgs"
+            "home-manager"
+            "nix-darwin"
+            "nixos-flake"
+            "nix-index-database"
+            "nixvim"
+          ];
+        };
 
         treefmt.config = {
           projectRootFile = "flake.nix";
