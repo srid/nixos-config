5fdf9de526de41eb62859ead7726d72bb25c9017
Sridhar Ratnakumar <srid@srid.ca>
2024-06-17 15:51:13 -0400

    ci: only on nixos-config for now.

--- clusters/github-runner/repos.nix
+++ clusters/github-runner/repos.nix
@@ -2,11 +2,11 @@
   # My personal repos with self-hosted runners:
   # `num` is usually based on matrix configuration size
   srid = {
-    haskell-flake.num = 2 * 7;
     nixos-config.num = 2;
-    nixos-flake.num = 2 * 5;
-    perpetuum.num = 2;
-    emanote.num = 2;
-    nixci.num = 2;
+    # TODO: Moving these to Juspay
+    # nixci.num = 2;
+    # haskell-flake.num = 2 * 7;
+    # nixos-flake.num = 2 * 5;
+    # emanote.num = 2;
   };
 }
