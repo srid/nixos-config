2dc01c3d36245bd5c7056940830e2f3727ea5f96
Sridhar Ratnakumar <srid@srid.ca>
2024-06-21 23:45:15 -0400

    flake: Remove unused systems

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
   outputs = inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.nixos-flake.flakeModule
