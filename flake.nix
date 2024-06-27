030d14cc4a00189af588b1e00ef8eef516bea714
Sridhar Ratnakumar <srid@srid.ca>
2024-06-27 17:39:27 -0400

    allow arm

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
   outputs = inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.nixos-flake.flakeModule
