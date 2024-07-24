a20e90cfdd354ce8a7c2fe70ecc27664ef241b72
Sridhar Ratnakumar <srid@srid.ca>
2024-07-24 17:27:27 -0400

    Add omnix cache

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -1,12 +1,10 @@
 {
   nix.settings.trusted-public-keys = [
-    #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
     "nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
-    # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
   nix.settings.substituters = [
-    #"https://nix-community.cachix.org"
-    # "https://cache.garnix.io?priority=41"
+    "https://om.cachix.org"
     "https://nammayatri.cachix.org?priority=42"
   ];
 }
