78e101f434e8290a60ab66c18ff265fc1ea686d4
Sridhar Ratnakumar <srid@srid.ca>
2024-08-04 13:55:35 -0400

    Prevent nix.conf in $HOME (cachix being offender)
    Caches are managed globally

--- home/nix.nix
+++ home/nix.nix
@@ -1,4 +1,8 @@
 {
   # Garbage collect automatically every week
   nix.gc.automatic = true;
+
+  # Nix configuration is managed globally by nix-darwin.
+  # Prevent $HOME nix.conf from disrespecting it.
+  home.file."~/.config/nix/nix.conf".text = "";
 }
