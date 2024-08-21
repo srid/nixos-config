5460bc542f82b3f15e1a402c4f9b8d20b7283cee
Sridhar Ratnakumar <srid@srid.ca>
2024-08-21 12:41:35 -0400

    github: don't build nixos-config

--- nixos/github-runner.nix
+++ nixos/github-runner.nix
@@ -2,7 +2,7 @@
   services.github-nix-ci = {
     age.secretsDir = ../secrets;
     personalRunners = {
-      "srid/nixos-config".num = 2;
+      # "srid/nixos-config".num = 2;
       "srid/actualism-app".num = 1;
     };
   };
