2e6cada89a7c9a6f47fb61992d57377ed6e6e1b0
Sridhar Ratnakumar <srid@srid.ca>
2024-08-22 11:34:16 -0400

    Upgrade to latest Nix (2.23)

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -14,11 +14,15 @@
   };
 
   nix = {
+    # Choose from https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=nix
+    package = pkgs.nixVersions.latest;
+
     nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
+
     settings = {
       max-jobs = "auto";
-      experimental-features = "nix-command flakes repl-flake";
+      experimental-features = "nix-command flakes";
       # I don't have an Intel mac.
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
       # Nullify the registry for purity.
