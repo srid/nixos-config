579b6920d6491d722117851c0487ab3c4d622642
Sridhar Ratnakumar <srid@srid.ca>
2024-08-26 12:29:22 -0400

    Revert

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,8 +1,6 @@
-{ config, lib, ... }:
 {
   programs.direnv = {
     enable = true;
-    package = lib.mkIf (config.nix.package != null) config.nix.package;
     nix-direnv.enable = true;
     config.global = {
       hide_env_diff = true;
