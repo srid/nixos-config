6c59f978bc8fc04a1c0c95c28299117794118964
Sridhar Ratnakumar <srid@srid.ca>
2024-08-26 14:28:46 -0400

    nix-direnv: set package because hm doesn't

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,7 +1,13 @@
+{ config, pkgs, ... }:
+
 {
   programs.direnv = {
     enable = true;
-    nix-direnv.enable = true;
+    nix-direnv = {
+      enable = true;
+      # Until https://github.com/juspay/nix-dev-home/issues/68
+      package = pkgs.nix-direnv.override { nix = config.nix.package; };
+    };
     config.global = {
       hide_env_diff = true;
     };
