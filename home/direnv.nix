a8f112e759d546f16d13463319c5463beb7f539a
Sridhar Ratnakumar <srid@srid.ca>
2024-08-27 16:10:36 -0400

    nix-direnv: keep package option until this PR
    https://github.com/nix-community/home-manager/pull/5773

--- home/direnv.nix
+++ home/direnv.nix
@@ -5,7 +5,7 @@
     enable = true;
     nix-direnv = {
       enable = true;
-      # Until https://github.com/juspay/nix-dev-home/issues/68
+      # Until https://github.com/nix-community/home-manager/pull/5773
       package = lib.mkIf (config.nix.package != null)
         (pkgs.nix-direnv.override { nix = config.nix.package; });
     };
