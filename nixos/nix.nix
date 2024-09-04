b2ba4bbdba074584190f41c1fa1272f245e544bc
Sridhar Ratnakumar <srid@srid.ca>
2024-09-03 22:08:45 -0400

    Don't use unstable Nix
    It segfaults

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -15,7 +15,7 @@
 
   nix = {
     # Choose from https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=nix
-    package = pkgs.nixVersions.latest;
+    # package = pkgs.nixVersions.latest;
 
     nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
