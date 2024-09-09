d725f603859afa283e3b2991307757caae36715e
Sridhar Ratnakumar <srid@srid.ca>
2024-09-09 13:17:35 -0400

    darwin: configure dock & finder

--- systems/darwin.nix
+++ systems/darwin.nix
@@ -22,6 +22,27 @@ in
     home = "/Users/${flake.config.people.myself}";
   };
 
+  # See https://github.com/ryan4yin/nix-darwin-kickstarter/blob/main/rich-demo/modules/system.nix
+  system.defaults = {
+    dock = {
+      autohide = true;
+      # customize Hot Corners(触发角, 鼠标移动到屏幕角落时触发的动作)
+      wvous-tl-corner = 2; # top-left - Mission Control
+      wvous-tr-corner = 13; # top-right - Lock Screen
+      wvous-bl-corner = 3; # bottom-left - Application Windows
+      wvous-br-corner = 4; # bottom-right - Desktop
+    };
+
+    finder = {
+      _FXShowPosixPathInTitle = true; # show full path in finder title
+      AppleShowAllExtensions = true; # show all file extensions
+      FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+      QuitMenuItem = true; # enable quit menu item
+      ShowPathbar = true; # show path bar
+      ShowStatusBar = true; # show status bar
+    };
+  };
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
