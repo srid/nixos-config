d9e47665619c6d3a232490f45f73e2706ed85dad
Sridhar Ratnakumar <srid@srid.ca>
2024-09-25 16:31:22 -0400

    zsh: use sessionPath
    resolves #22

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,6 +1,11 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
+  home.sessionPath = [
+    "/etc/profiles/per-user/$USER/bin"
+    "/nix/var/nix/profiles/system/sw/bin"
+    "/usr/local/bin"
+  ];
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
@@ -14,8 +19,6 @@
     ];
 
     envExtra = ''
-      export PATH=/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/system/sw/bin:/usr/local/bin:$PATH
-
       # Because, adding it in .ssh/config is not enough.
       # cf. https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
       export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
