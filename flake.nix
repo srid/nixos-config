f758380c0dd0188578c306b04283d2544682dd88
Sridhar Ratnakumar <srid@srid.ca>
2024-09-26 17:11:08 -0400

    nixos-flake: back to master

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixos-flake.url = "github:srid/nixos-flake/dont-output-modules";
+    nixos-flake.url = "github:srid/nixos-flake";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     ragenix.url = "github:yaxitech/ragenix";
