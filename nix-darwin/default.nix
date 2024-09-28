d88e1335dfa8643222c95444c73b512e0525a7a3
Sridhar Ratnakumar <srid@srid.ca>
2024-09-28 10:52:07 -0400

    my-home is not used outside the file; remove

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -3,16 +3,14 @@
   # Configuration common to all macOS systems
   flake = {
     darwinModules = {
-      my-home = {
-        home-manager.users.${config.people.myself} = {
-          imports = [
-            self.homeModules.common-darwin
-          ];
-        };
-      };
-
       default.imports = [
-        self.darwinModules.my-home
+        {
+          home-manager.users.${config.people.myself} = {
+            imports = [
+              self.homeModules.common-darwin
+            ];
+          };
+        }
         self.nixosModules.common
         inputs.ragenix.darwinModules.default
         inputs.github-nix-ci.darwinModules.default
--- nixos/default.nix
+++ nixos/default.nix
@@ -5,23 +5,22 @@
   flake = {
     nixosModules = {
       # NixOS modules that are known to work on nix-darwin.
+      # Thsi is shared with nix-darwin/default.nix
       common.imports = [
         ./nix.nix
         ./caches
         ./self/primary-as-admin.nix
       ];
 
-      my-home = {
-        users.users.${config.people.myself}.isNormalUser = true;
-        home-manager.users.${config.people.myself} = {
-          imports = [
-            self.homeModules.common-linux
-          ];
-        };
-      };
-
       default.imports = [
-        self.nixosModules.my-home
+        {
+          users.users.${config.people.myself}.isNormalUser = true;
+          home-manager.users.${config.people.myself} = {
+            imports = [
+              self.homeModules.common-linux
+            ];
+          };
+        }
         self.nixosModules.common
         inputs.ragenix.nixosModules.default
         inputs.github-nix-ci.nixosModules.default
