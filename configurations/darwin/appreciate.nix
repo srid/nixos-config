68579d16bb4dd643eb97421d662fbf0b0f48739d
Sridhar Ratnakumar <srid@srid.ca>
2024-10-10 10:58:51 -0400

    darwin: Disable some hot corners

--- configurations/darwin/appreciate.nix
+++ configurations/darwin/appreciate.nix
@@ -26,9 +26,9 @@ in
       autohide = true;
       # customize Hot Corners(触发角, 鼠标移动到屏幕角落时触发的动作)
       wvous-tl-corner = 2; # top-left - Mission Control
-      wvous-tr-corner = 13; # top-right - Lock Screen
+      # wvous-tr-corner = 13; # top-right - Lock Screen
       wvous-bl-corner = 3; # bottom-left - Application Windows
-      wvous-br-corner = 4; # bottom-right - Desktop
+      # wvous-br-corner = 4; # bottom-right - Desktop
     };
 
     finder = {
