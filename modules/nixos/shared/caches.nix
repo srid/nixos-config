c49e03e66819f53b33e6a58e8f76e3fefd74d525
Sridhar Ratnakumar <srid@srid.ca>
2024-10-17 14:09:47 -0400

    cache: unused for now

--- modules/nixos/shared/caches.nix
+++ modules/nixos/shared/caches.nix
@@ -1,10 +1,8 @@
 {
   nix.settings.trusted-public-keys = [
     "om.cachix.org-1:ifal/RLZJKN4sbpScyPGqJ2+appCslzu7ZZF/C01f2Q="
-    "nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
   ];
   nix.settings.substituters = [
     "https://om.cachix.org"
-    "https://nammayatri.cachix.org?priority=42"
   ];
 }
