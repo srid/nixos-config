6574f7216e2e6493e90fa57928920bf1d25afda1
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 16:44:49 -0400

    Workaround a NetworkManager issue on switch
    https://github.com/NixOS/nixpkgs/issues/180175

--- configurations/nixos/vixen/default.nix
+++ configurations/nixos/vixen/default.nix
@@ -17,4 +17,8 @@ in
   services.tailscale.enable = true;
 
   programs.nix-ld.enable = true; # for vscode server
+
+  # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
