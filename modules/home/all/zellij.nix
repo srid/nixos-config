606448ef06935842fd0952ad4be784c1062db84d
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 18:10:30 -0400

    zellij: add theme

--- modules/home/all/zellij.nix
+++ modules/home/all/zellij.nix
@@ -4,8 +4,9 @@
   programs.zellij = {
     enable = true;
     settings = {
-      theme = if pkgs.system == "aarch64-darwin" then "dracula" else "default";
+      theme = if pkgs.system == "aarch64-darwin" then "dracula" else "gruvbox-light";
       # https://github.com/nix-community/home-manager/issues/3854
+      # https://github.com/zellij-org/zellij/blob/main/zellij-utils/assets/themes/gruvbox.kdl
       themes.dracula = {
         fg = [ 248 248 242 ];
         bg = [ 40 42 54 ];
@@ -18,6 +19,19 @@
         cyan = [ 139 233 253 ];
         white = [ 255 255 255 ];
         orange = [ 255 184 108 ];
+      };
+      themes.gruvbox-light = {
+        fg = [ 124 111 100 ];
+        bg = [ 251 82 75 ];
+        black = [ 40 40 40 ];
+        red = [ 205 75 69 ];
+        green = [ 152 151 26 ];
+        yellow = [ 215 153 33 ];
+        blue = [ 69 133 136 ];
+        magenta = [ 177 98 134 ];
+        cyan = [ 104 157 106 ];
+        white = [ 213 196 161 ];
+        orange = [ 214 93 14 ];
       };
     };
   };
