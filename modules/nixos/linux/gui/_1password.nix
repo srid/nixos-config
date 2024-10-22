b8243a5f478c1c9ddaf822962fee42cd944c14eb
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 18:05:06 -0400

    vixen: add 1password gui

---
+++ modules/nixos/linux/gui/_1password.nix
@@ -0,0 +1,19 @@
+# https://nixos.wiki/wiki/1Password
+{ lib, ... }:
+{
+    # Enable the unfree 1Password packages
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password-gui"
+    "1password"
+  ];
+  # Alternatively, you could also just allow all unfree packages
+  # nixpkgs.config.allowUnfree = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "srid" ];
+  };
+}
