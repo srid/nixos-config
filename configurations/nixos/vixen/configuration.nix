b3928c9b5d762f3cf6799040bedf37c49ea4fa70
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 15:37:42 -0400

    Fix gnome-tweaks warning

--- configurations/nixos/vixen/configuration.nix
+++ configurations/nixos/vixen/configuration.nix
@@ -83,7 +83,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     brave
-    gnome.gnome-tweaks
+    gnome-tweaks
     vscode
   ];
 
