58db830d3a3f21b0b1ed0613b2852e4823060d65
Sridhar Ratnakumar <srid@srid.ca>
2024-10-22 20:34:33 -0400

    allowUnfree is already set

--- modules/nixos/linux/gui/_1password.nix
+++ modules/nixos/linux/gui/_1password.nix
@@ -1,15 +1,6 @@
 # https://nixos.wiki/wiki/1Password
 { lib, ... }:
 {
-  # Enable the unfree 1Password packages
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "1password-gui"
-    "1password-cli"
-    "1password"
-  ];
-  # Alternatively, you could also just allow all unfree packages
-  # nixpkgs.config.allowUnfree = true;
-
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
