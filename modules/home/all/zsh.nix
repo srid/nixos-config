5941c427ea945b14fedba82a1860eebb05033e57
Sridhar Ratnakumar <srid@srid.ca>
2024-10-23 21:39:04 -0400

    enable zsh

--- modules/home/all/zsh.nix
+++ modules/home/all/zsh.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  home.sessionPath = [
+  home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
     "/etc/profiles/per-user/$USER/bin"
     "/nix/var/nix/profiles/system/sw/bin"
     "/usr/local/bin"
@@ -18,7 +18,7 @@
       }
     ];
 
-    envExtra = ''
+    envExtra = lib.mkIf pkgs.stdenv.isDarwin ''
       # Because, adding it in .ssh/config is not enough.
       # cf. https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
       export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
--- modules/home/linux-only.nix
+++ modules/home/linux-only.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./all/bash.nix
+    ./all/zsh.nix
     ./all/vscode-server.nix
     ./all/rio.nix
   ];
