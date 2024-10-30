99a1b38f6c6bf50a47cff6026dedd8eab7f50ca4
Sridhar Ratnakumar <srid@srid.ca>
2024-10-30 15:42:16 -0400

    hyprland: cursor

--- modules/nixos/linux/gui/hyprland.nix
+++ modules/nixos/linux/gui/hyprland.nix
@@ -16,6 +16,32 @@ in
     services.dunst.enable = true;
     programs.hyprlock.enable = true;
     home.sessionVariables.NIXOS_OZONE_WL = "1";
+    # https://wiki.hyprland.org/0.41.0/Nix/Hyprland-on-Home-Manager/#fixing-problems-with-themes
+    home.pointerCursor = {
+      gtk.enable = true;
+      # x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 16;
+    };
+
+    gtk = {
+      enable = true;
+      theme = {
+        package = pkgs.flat-remix-gtk;
+        name = "Flat-Remix-GTK-Grey-Darkest";
+      };
+
+      iconTheme = {
+        package = pkgs.gnome.adwaita-icon-theme;
+        name = "Adwaita";
+      };
+
+      font = {
+        name = "Sans";
+        size = 11;
+      };
+    };
   }];
   environment.systemPackages = with pkgs; [
     kitty
