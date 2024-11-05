077d1f7a6434b40035e61472c26144be20e0fc4f
Sridhar Ratnakumar <srid@srid.ca>
2024-11-05 15:13:41 -0500

    i2c

--- configurations/nixos/vixen/default.nix
+++ configurations/nixos/vixen/default.nix
@@ -30,6 +30,8 @@ in
     telegram-desktop
   ];
 
+  hardware.i2c.enable = true;
+
   # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
   # https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
