9ebe41ddc5a447acf6456fe634bdd7329c1a9c50
Sridhar Ratnakumar <srid@srid.ca>
2024-11-05 16:28:00 -0500

    hyprland: animations

--- modules/nixos/linux/gui/hyprland/settings.nix
+++ modules/nixos/linux/gui/hyprland/settings.nix
@@ -102,6 +102,36 @@ in
       };
     };
 
+    animations = {
+      enabled = true;
+      # Default animations
+      bezier = [
+        "easeOutQuint,0.23,1.0,0.32,1"
+        "easeInOutCubic,0.65,0.05,0.36,1"
+        "linear,0,0,1,1"
+        "almostLinear,0.5,0.5,0.75,1.0"
+        "quick,0.15,0.9,1,1"
+      ];
+      animation = [
+        "global, 1, 10, default"
+        "border, 1, 5.39, easeOutQuint"
+        "windows, 1, 4.79, easeOutQuint"
+        "windowsIn, 1, 4.79, easeOutQuint, popin 87%"
+        "windowsOut, 1, 4.79, linear, popin 87%"
+        "fadeIn, 1, 1.73, almostLinear"
+        "fadeOut, 1, 1.46, almostLinear"
+        "fade, 1, 3.03, quick"
+        "layers, 1, 3.81, easeOutQuint"
+        "layersIn, 1, 4, easeOutQuint, fade"
+        "layersOut, 1, 5, linear, fade"
+        "fadeLayersIn, 1, 1.79, almostLinear"
+        "fadeLayersOut, 1, 1.39, almostLinear"
+        "workspaces, 1, 1.94, almostLinear, fade"
+        "workspacesIn, 1, 1.21, almostLinear, fade"
+        "workspacesOut, 1, 1.94, almostLinear, fade"
+      ];
+    };
+
     misc = {
       force_default_wallpaper = 2;
     };
