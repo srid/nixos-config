dbbbc79585ed50022c6ffc9c26f8df37b493c4f7
Sridhar Ratnakumar <srid@srid.ca>
2024-11-11 14:46:40 -0500

    disable pulseaudoi

--- configurations/nixos/vixen/configuration.nix
+++ configurations/nixos/vixen/configuration.nix
@@ -42,7 +42,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
