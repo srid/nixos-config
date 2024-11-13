4a1b3dcd628820c81181db4360f8bb1e62ebf0ee
Sridhar Ratnakumar <srid@srid.ca>
2024-11-13 17:05:50 -0500

    emacs: fonts

--- doom.d/init.el
+++ doom.d/init.el
@@ -36,7 +36,7 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
