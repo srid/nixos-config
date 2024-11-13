ee49874e6bce2f866a31ff9a293e6d1663681eec
Sridhar Ratnakumar <srid@srid.ca>
2024-11-13 17:00:10 -0500

    emacs: direnv

--- doom.d/init.el
+++ doom.d/init.el
@@ -90,7 +90,7 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
+       direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
