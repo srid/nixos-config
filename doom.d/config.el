e8d80b690c1d47dfc80a0099f49573ca188303c7
Sridhar Ratnakumar <srid@srid.ca>
2024-11-18 12:04:59 -0500

    doom: disable

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Fira Code" :size 13))
+;; (setq doom-font (font-spec :family "Fira Code" :size 13))
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
