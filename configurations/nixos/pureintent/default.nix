5fc59d8bc8deb2724dbaf8283ff23af396588ae4
Sridhar Ratnakumar <srid@srid.ca>
2024-11-23 12:11:56 -0500

    navidrome

--- configurations/nixos/pureintent/default.nix
+++ configurations/nixos/pureintent/default.nix
@@ -20,6 +20,15 @@ in
     enable = true;
     package = pkgs.netdataCloud;
   };
+  services.navidrome = {
+    enable = true;
+    settings = {
+      Address = "100.113.68.55";
+      Port = 4533;
+      MusicFolder = "/var/lib/navidrome/Music";
+    };
+    openFirewall = true;
+  };
 
   programs.nix-ld.enable = true; # for vscode server
 
