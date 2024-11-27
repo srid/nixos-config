f7c04083769e80aecb797ddf77c31d75151bacd8
Sridhar Ratnakumar <srid@srid.ca>
2024-11-27 14:57:18 -0500

    cleanup

--- modules/home/all/juspay.nix
+++ modules/home/all/juspay.nix
@@ -1,45 +1,7 @@
 # For Juspay work
-let
-  # I don't care to connect to VPN on my macbook
-  # So, I'll clone through an office machine
-  gitCloneThrough = { host, user }:
-    let
-      port = 5445;
-      gitSshRemote = "ssh.bitbucket.juspay.net";
-    in
-    {
-      programs.ssh.matchBlocks = {
-        ${host} = {
-          inherit user;
-          dynamicForwards = [{ inherit port; }];
-        };
-        /*
-        ${gitSshRemote} = {
-          user = "git";
-          proxyCommand = "nc -x localhost:${builtins.toString port} %h %p";
-        };
-        */
-      };
-    };
-in
 {
-  imports = [
-    (gitCloneThrough { host = "vanjaram"; user = "srid"; })
-  ];
   programs.ssh = {
     matchBlocks = {
-      # Juspay machines (through Tailscale)
-      vanjaram = {
-        hostname = "100.83.79.127";
-        user = "srid";
-        forwardAgent = true;
-      };
-      biryani = {
-        hostname = "100.97.32.60";
-        user = "admin";
-        forwardAgent = true;
-      };
-
       # To clone Juspay repos.
       # https://developer.1password.com/docs/ssh/agent/advanced/#match-key-with-host
       "bitbucket.org" = {
