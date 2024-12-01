6152244e764ea4f6a3f1c28f0d1a4d1ca3596ed3
Sridhar Ratnakumar <srid@srid.ca>
2024-12-01 15:30:14 -0500

    add terms

--- modules/home/linux-only.nix
+++ modules/home/linux-only.nix
@@ -3,8 +3,12 @@
     ./all/bash.nix
     ./all/zsh.nix
     ./all/vscode-server.nix
-    ./all/rio.nix
     ./all/emacs.nix
     ./all/obs.nix
+
+    # Terms
+    ./all/rio.nix
+    ./all/wezterm
+    ./all/kitty.nix
   ];
 }
