6675d0b291a2956fb6a79004e900948bb5b6bac4
Sridhar Ratnakumar <srid@srid.ca>
2025-01-18 14:19:59 -0500

    secrets: add naivete

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,8 @@ let
 
   appreciate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICra+ZidiwrHGjcGnyqPvHcZDvnGivbLMayDyecPYDh0";
   pureintent = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkY5feaNt4elPqRQimB9h3OFxtFAzp98p1H+JezBv92 root@nixos";
-  systems = [ appreciate pureintent ];
+  naivete = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXQ4jgsoM0K+6oddNCerjVFDibQcv7JWwc5OsT8AkGa";
+  systems = [ appreciate pureintent naivete ];
 in
 {
   "hedgedoc.env.age".publicKeys = users ++ systems;
