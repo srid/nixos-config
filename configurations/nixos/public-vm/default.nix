82c121e064a55c3c2e4e941bd4089afcac33cee2
Sridhar Ratnakumar <srid@srid.ca>
2025-01-21 15:54:53 -0500

    better

--- configurations/nixos/public-vm/default.nix
+++ configurations/nixos/public-vm/default.nix
@@ -1,24 +1,5 @@
-{ flake, pkgs, ... }:
-
-let
-  inherit (flake) inputs;
-  inherit (inputs) self;
-in
 {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/virtualisation/lxd-virtual-machine.nix"
-    (self + /modules/nixos/shared/primary-as-admin.nix)
+    ./configuration.nix
   ];
-
-  nixpkgs.hostPlatform = "x86_64-linux";
-  services.openssh.settings.PasswordAuthentication = false;
-
-  # Tailscale to connect to the VM from anywhere
-  services.tailscale.enable = true;
-
-  # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
-  # https://github.com/NixOS/nixpkgs/issues/180175
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  system.stateVersion = "24.11";
 }
