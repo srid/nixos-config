c45943d159a5cb66d7076d720740a3f7723132fe
Sridhar Ratnakumar <srid@srid.ca>
2025-01-21 15:42:26 -0500

    o

--- modules/flake-parts/incus-image.nix
+++ modules/flake-parts/incus-image.nix
@@ -6,12 +6,12 @@
         NAME=$1
 
         echo "Building image ... "
-        METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/nixos*.tar.xz
+        METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/
         IMG=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.qemuImage)/nixos.qcow2
 
         echo "Importing ... "
         set -x
-        sudo incus image import --alias srid/"$NAME" "$METADATA" "$IMG"
+        sudo incus image import --alias srid/"$NAME" "$METADATA"/*.tar.xz "$IMG"
       '';
     };
   };
