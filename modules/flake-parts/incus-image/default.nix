d6a15f13a324e1e8fe97d4382520b1e5aadc92e1
Sridhar Ratnakumar <srid@srid.ca>
2025-02-01 14:31:04 -0500

    fmt

--- modules/flake-parts/incus-image/default.nix
+++ modules/flake-parts/incus-image/default.nix
@@ -4,30 +4,30 @@
       incus-image-vm-import.program = pkgs.writeShellApplication {
         name = "incus-image-vm-import";
         text = ''
-            NAME=$1
+          NAME=$1
 
-            echo "Building image ... "
-            METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/
-            IMG=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.qemuImage)/nixos.qcow2
+          echo "Building image ... "
+          METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/
+          IMG=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.qemuImage)/nixos.qcow2
 
-            echo "Importing ... "
-            set -x
-            sudo incus image import --alias srid/"$NAME" "$METADATA"/*.tar.xz "$IMG"
+          echo "Importing ... "
+          set -x
+          sudo incus image import --alias srid/"$NAME" "$METADATA"/*.tar.xz "$IMG"
         '';
       };
 
       incus-image-container-import.program = pkgs.writeShellApplication {
         name = "incus-image-container-import";
         text = ''
-            NAME=$1
+          NAME=$1
 
-            echo "Building image ... "
-            METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/
-            IMG=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.squashfs)/nixos-lxc-image-${system}.squashfs
+          echo "Building image ... "
+          METADATA=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.metadata)/tarball/
+          IMG=$(nix build --no-link --print-out-paths ${inputs.self}#nixosConfigurations."$NAME".config.system.build.squashfs)/nixos-lxc-image-${system}.squashfs
 
-            echo "Importing ... "
-            set -x
-            sudo incus image import --alias srid/"$NAME" "$METADATA"/*.tar.xz "$IMG"
+          echo "Importing ... "
+          set -x
+          sudo incus image import --alias srid/"$NAME" "$METADATA"/*.tar.xz "$IMG"
         '';
       };
     };
