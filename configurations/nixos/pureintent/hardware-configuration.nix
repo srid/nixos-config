3e918ef856a9d70e489663e1d00d6ba226ba73a1
Sridhar Ratnakumar <srid@srid.ca>
2025-03-20 18:07:01 -0400

    pureintent: enable swap

--- configurations/nixos/pureintent/hardware-configuration.nix
+++ configurations/nixos/pureintent/hardware-configuration.nix
@@ -27,7 +27,9 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/var/lib/swapfile"; size = 32 * 1024; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
