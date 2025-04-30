bdf96bdd37903b86b5e54796ce355797bed42a6b
Sridhar Ratnakumar <srid@srid.ca>
2025-04-30 17:32:32 +1000

    Disable hyprland cache

--- modules/nixos/shared/caches.nix
+++ modules/nixos/shared/caches.nix
@@ -1,8 +1,8 @@
 {
   nix.settings.trusted-public-keys = [
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
   ];
   nix.settings.substituters = [
-    "https://hyprland.cachix.org"
+    # "https://hyprland.cachix.org"
   ];
 }
