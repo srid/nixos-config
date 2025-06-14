e33a3bc4ff508fe2d054b504177a6f31d1727bae
Sridhar Ratnakumar <srid@srid.ca>
2025-06-14 16:30:51 +1000

    omnix: use from nixpkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,6 +13,6 @@ self: super: {
   copy-md-as-html = self.callPackage "${packages}/copy-md-as-html.nix" { };
   ci = self.callPackage "${packages}/ci" { };
   touchpr = self.callPackage "${packages}/touchpr" { };
-  omnix = inputs.omnix.packages.${self.system}.default;
+  # omnix = inputs.omnix.packages.${self.system}.default;
   git-merge-and-delete = self.callPackage "${packages}/git-merge-and-delete.nix" { };
 }
