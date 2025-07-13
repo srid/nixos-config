59316343f762c2c90330bea3b4b069ac148456ca
Sridhar Ratnakumar <srid@srid.ca>
2025-07-13 17:55:10 -0400

    Nah, ragenix is broken on macOS

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nixos-unified.url = "github:srid/nixos-unified";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    agenix.url = "github:yaxitech/ragenix";
+    agenix.url = "github:ryantm/agenix";
     nuenv.url = "github:hallettj/nuenv/writeShellApplication";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
