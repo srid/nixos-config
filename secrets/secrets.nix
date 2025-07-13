22e8fafbd07a64eb07efe700ec80b3be989a93a8
Sridhar Ratnakumar <srid@srid.ca>
2025-07-13 17:49:42 -0400

    secrets: add infinitude

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,8 @@ let
   appreciate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICra+ZidiwrHGjcGnyqPvHcZDvnGivbLMayDyecPYDh0";
   pureintent = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkY5feaNt4elPqRQimB9h3OFxtFAzp98p1H+JezBv92 root@nixos";
   naivete = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXQ4jgsoM0K+6oddNCerjVFDibQcv7JWwc5OsT8AkGa";
-  systems = [ appreciate pureintent naivete ];
+  infinitude = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjxxIse4V0mC3qqhPdcVp1dp2R8fQr2AMcf/fd9vWcj";
+  systems = [ appreciate pureintent naivete infinitude ];
 in
 {
   "hedgedoc.env.age".publicKeys = users ++ systems;
