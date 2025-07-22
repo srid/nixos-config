c6c157913e49056931abd5dda8349527a7c7c23c
Sridhar Ratnakumar <srid@srid.ca>
2025-07-22 10:27:31 -0400

    Add home configuration (macOS)

---
+++ configurations/home/srid.nix
@@ -0,0 +1,13 @@
+{ flake, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    self.homeModules.default
+    self.homeModules.darwin-only
+  ];
+
+  home.username = "srid";
+}
