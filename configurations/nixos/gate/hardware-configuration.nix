f5f69ae4bac405d46c738d73341ac289c1db59d9
Sridhar Ratnakumar <srid@srid.ca>
2025-07-27 16:21:06 -0400

    cleanup

--- configurations/nixos/gate/hardware-configuration.nix
+++
@@ -1,9 +0,0 @@
-{ modulesPath, ... }:
-{
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader.grub.device = "/dev/sda";
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-
-}
---
+++ configurations/nixos/vixen/README.md
@@ -0,0 +1,3 @@
+## Thinkpad P14s NixOS configuration
+
+I currently don't use this. The P14s is running Windows for occasional gaming.
