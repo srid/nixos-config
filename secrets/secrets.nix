59f3771bfff8ddb29fe6606f4980ddd6f7cc8e79
Sridhar Ratnakumar <srid@srid.ca>
2025-07-27 13:16:43 -0400

    update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,9 +5,8 @@ let
   appreciate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICra+ZidiwrHGjcGnyqPvHcZDvnGivbLMayDyecPYDh0";
   pureintent = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkY5feaNt4elPqRQimB9h3OFxtFAzp98p1H+JezBv92 root@nixos";
   naivete = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXQ4jgsoM0K+6oddNCerjVFDibQcv7JWwc5OsT8AkGa";
-  infinitude = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjxxIse4V0mC3qqhPdcVp1dp2R8fQr2AMcf/fd9vWcj";
-  infinitude-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBF5YxbeuZhn20pxbmHmA8UU9sWTmvQ/lcKkfY9A+Snx";
-  systems = [ appreciate pureintent naivete infinitude infinitude-nixos ];
+  infinitude-macos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjg6aknmaXdQ/arHcTD+USFwCTsUGyJv9R1dXnejdby";
+  systems = [ appreciate pureintent naivete infinitude-macos ];
 in
 {
   "hedgedoc.env.age".publicKeys = users ++ systems;
