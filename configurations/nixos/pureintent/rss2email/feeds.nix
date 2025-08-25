e2b05c93309f856a668e71bc51ba7d6b056c2d00
Sridhar Ratnakumar <srid@srid.ca>
2025-08-24 21:06:26 -0400

    some feeds

--- configurations/nixos/pureintent/rss2email/feeds.nix
+++ configurations/nixos/pureintent/rss2email/feeds.nix
@@ -1,21 +1,19 @@
 let
-  redditTop = subreddit: limit: period: {
-    url = "https://www.reddit.com/r/${subreddit}/top/.rss?t=${period}&limit=${toString limit}";
-  };
+  redditTop = subreddit: limit: period:
+    "https://www.reddit.com/r/${subreddit}/top/.rss?t=${period}&limit=${toString limit}";
 in
 {
-  nixos-logo-proposal = {
-    url = "https://discourse.nixos.org/t/proposal-update-the-nixos-logo-for-ukrainian-flag-day-23-august/68375.rss";
-  };
-  hackernews-best = {
-    url = "https://hnrss.org/best?points=50"; # Posts with 50+ points
-  };
+  # actualism
+  vineeto-discourse.url = "https://discuss.actualism.online/u/vineeto/activity.rss";
 
-  haskell-reddit-top = redditTop "haskell" 5 "day";
-  nixos-reddit-top = redditTop "nixos" 5 "day";
-  scientificnutrition-reddit-top = redditTop "scientificnutrition" 5 "day";
+  # general tech news
+  hackernews-best.url = "https://hnrss.org/best?points=50";
+  notebookcheck.url = "https://www.notebookcheck.net/RSS-Feed-Notebook-Reviews.8156.0.html";
 
-  notebookcheck = {
-    url = "https://www.notebookcheck.net/RSS-Feed-Notebook-Reviews.8156.0.html";
-  };
+  # reddit
+  haskell-reddit-top.url = redditTop "haskell" 5 "day";
+  nixos-reddit-top.url = redditTop "nixos" 5 "day";
+
+  # woke drama
+  nixos-drupol-discourse-ukraine.url = "https://discourse.nixos.org/t/proposal-update-the-nixos-logo-for-ukrainian-flag-day-23-august/68375.rss";
 }
