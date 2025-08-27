347c39e9e684061fa3b534d42ed419235228df48
Sridhar Ratnakumar <srid@srid.ca>
2025-08-28 03:56:19 +0530

    bash: improv

--- modules/home/all/bash.nix
+++ modules/home/all/bash.nix
@@ -1,3 +1,15 @@
 {
-  programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    enableVteIntegration = true; # Terminal integration for working directory tracking
+    historyControl = [ "ignoredups" "ignorespace" ]; # Don't save duplicate commands or commands starting with space
+    historySize = 10000; # Keep more commands in memory for better recall
+    historyFileSize = 100000; # Persist more commands to disk for long-term history
+    shellOptions = [
+      "histappend" # Append to history file instead of overwriting
+      "checkwinsize" # Check window size after each command
+      "cdspell" # Correct minor spelling errors in cd commands
+    ];
+  };
 }
