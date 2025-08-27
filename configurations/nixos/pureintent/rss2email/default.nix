cbd6255cd00f571bee3383f0dc4d6d28b18cb207
Sridhar Ratnakumar <srid@srid.ca>
2025-08-27 10:10:44 -0400

    1d

--- configurations/nixos/pureintent/rss2email/default.nix
+++ configurations/nixos/pureintent/rss2email/default.nix
@@ -4,7 +4,7 @@
   services.rss2email = {
     enable = true;
     to = "srid@srid.ca";
-    interval = "1h"; # Check every hour
+    interval = "1d"; # Check every day
     config = {
       digest = true;
       digest-type = "multipart/mixed"; # Put content in email body instead of .eml attachments
