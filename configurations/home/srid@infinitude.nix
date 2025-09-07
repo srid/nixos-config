b6c809608b6eb5b2445b0e39b179fdbae64a0267
Sridhar Ratnakumar <srid@srid.ca>
2025-09-07 16:40:21 -0400

    1p

--- configurations/home/srid@infinitude.nix
+++ configurations/home/srid@infinitude.nix
@@ -15,4 +15,10 @@ in
   ] ++ lib.optionals pkgs.stdenv.isDarwin [
     pkgs.tart
   ];
+
+  # 1password ssh agent
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  };
+
 }
