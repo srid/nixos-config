79c2e500cdc7bc5b17b2ed5e5e1bf674a586243e
Sridhar Ratnakumar <srid@srid.ca>
2025-09-10 20:47:06 -0400

    nix: Fix path on ubuntu with official installer

--- modules/home/all/nix.nix
+++ modules/home/all/nix.nix
@@ -6,9 +6,12 @@
   home.file.".config/nix/nix.conf".text = "";
 
   # Global session path for all shells
-  home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
+  # Normally, we do this on macOS only. And it is not necessary on NixOS. Yet,
+  # non-NixOS Linux will need it (when using the official Nix installer).
+  home.sessionPath = [
     "/etc/profiles/per-user/$USER/bin"
     "/nix/var/nix/profiles/system/sw/bin"
+    "$HOME/.nix-profile/bin"
     "/usr/local/bin"
   ];
 }
