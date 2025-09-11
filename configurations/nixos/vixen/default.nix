207eaacf52754d3ed0729f9279adba33e0eecbc3
Sridhar Ratnakumar <srid@srid.ca>
2025-09-10 20:56:28 -0400

    Remove broken

--- configurations/nixos/vixen/default.nix
+++ configurations/nixos/vixen/default.nix
@@ -19,15 +19,6 @@ in
     vscode
   ];
 
-  # https://nixos.wiki/wiki/Steam
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.tailscale.enable = true;
 }
