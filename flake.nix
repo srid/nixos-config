14dc8801f68619ca1c73270ce6f7b4280192fec5
Sridhar Ratnakumar <srid@srid.ca>
2025-09-16 19:24:11 -0400

    vira: use pr

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       flake-parts.follows = "flake-parts";
     };
     try.url = "github:tobi/try";
-    vira.url = "github:juspay/vira";
+    vira.url = "github:juspay/vira/vira-hs-sq";
 
     # Neovim
     nixvim.url = "github:nix-community/nixvim";
