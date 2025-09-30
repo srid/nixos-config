d4a1698755c5669cd6f3e5f2e290e267e2276634
Sridhar Ratnakumar <srid@srid.ca>
2025-09-30 15:14:14 -0400

    zed: new model

--- configurations/home/srid@infinitude.nix
+++ configurations/home/srid@infinitude.nix
@@ -11,8 +11,8 @@
   # For Zed's Claude Code to work with Anthropic Vertex
   home.sessionVariables = {
     CLAUDE_CODE_USE_VERTEX = "1";
-    ANTHROPIC_VERTEX_PROJECT_ID = "dev-ai-delta";
-    ANTHROPIC_MODEL = "claude-sonnet-4"; # 4.5 not yet available
+    ANTHROPIC_VERTEX_PROJECT_ID = "dev-ai-gamma";
+    ANTHROPIC_MODEL = "claude-sonnet-4-5";
   };
 
   home.packages = [
