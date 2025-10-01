da86b1085dfd5c635cc299385471ccb049a70362
Sridhar Ratnakumar <srid@srid.ca>
2025-10-01 19:42:35 -0400

    pureintent: gnome

--- configurations/nixos/pureintent/default.nix
+++ configurations/nixos/pureintent/default.nix
@@ -61,6 +61,11 @@ in
 
   programs.nix-ld.enable = true; # for vscode server
 
+  # GNOME Desktop Environment
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
   # https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
