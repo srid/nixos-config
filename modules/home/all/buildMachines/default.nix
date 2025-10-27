ec07b8ccf149a2bfac10553892bd0d2bc5f21a79
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-10-27 11:29:41 -0400

    more generaly

--- modules/home/all/buildMachines/default.nix
+++ modules/home/all/buildMachines/default.nix
@@ -4,5 +4,10 @@
     package = lib.mkDefault pkgs.nix;
     enable = true;
     distributedBuilds = true;
+    extraOptions = ''
+      # Let remote builders download from cache directly.
+      # This is to avoid download-cum-copy on main builder.
+      builders-use-substitutes = true
+    '';
   };
 }
