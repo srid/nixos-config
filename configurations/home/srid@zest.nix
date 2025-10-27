5c866b03c6b189254c10462e331990d6d68a9842
Sridhar Ratnakumar <srid@srid.ca>
2025-10-27 15:14:36 -0400

    zest: enable remote builders

--- configurations/home/srid@zest.nix
+++ configurations/home/srid@zest.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, flake, ... }:
+{ flake, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+  homeMod = self + /modules/home;
+in
 {
   imports = [
     flake.inputs.self.homeModules.default
@@ -6,6 +11,10 @@
     (flake.inputs.self + /modules/home/all/1password.nix)
     (flake.inputs.self + /modules/home/all/juspay-vertex.nix)
     (flake.inputs.self + /modules/home/claude-code)
+
+    # Remote builders
+    "${homeMod}/all/buildMachines"
+    "${homeMod}/all/buildMachines/sincereintent.nix"
   ];
 
   home.username = "srid";
