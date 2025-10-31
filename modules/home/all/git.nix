a2e146ac60917daf55594cf4b8b1526a7b7cf409
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-10-31 13:18:41 -0400

    fix warnings

--- modules/home/all/git.nix
+++ modules/home/all/git.nix
@@ -6,31 +6,23 @@
 
   programs.git = {
     enable = true;
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        light = false;
-        side-by-side = true;
-        line-numbers = true;
-        pager = "${pkgs.less}/bin/less --mouse --wheel-lines=3";
-      };
-    };
-    userName = flake.config.me.fullname;
-    userEmail = flake.config.me.email;
-    aliases = {
-      co = "checkout";
-      ci = "commit";
-      cia = "commit --amend";
-      s = "status";
-      st = "status";
-      b = "branch";
-      # p = "pull --rebase";
-      pu = "push";
-    };
     ignores = [ "*~" "*.swp" ];
     lfs.enable = true;
-    extraConfig = {
+    settings = {
+      user = {
+        name = flake.config.me.fullname;
+        email = flake.config.me.email;
+      };
+      alias = {
+        co = "checkout";
+        ci = "commit";
+        cia = "commit --amend";
+        s = "status";
+        st = "status";
+        b = "branch";
+        # p = "pull --rebase";
+        pu = "push";
+      };
       init.defaultBranch = "master"; # Undo breakage due to https://srid.ca/luxury-belief
       #protocol.keybase.allow = "always";
       credential.helper = "store --file ~/.git-credentials";
@@ -41,6 +33,18 @@
       # Remember and auto-resolve merge conflicts
       # https://git-scm.com/book/en/v2/Git-Tools-Rerere
       rerere.enabled = true;
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      light = false;
+      side-by-side = true;
+      line-numbers = true;
+      pager = "${pkgs.less}/bin/less --mouse --wheel-lines=3";
     };
   };
 
