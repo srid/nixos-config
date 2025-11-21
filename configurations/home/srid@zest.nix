7a6a82b5dc65c7932d3b542a9ff3f6ae394b57c2
Sridhar Ratnakumar <srid@srid.ca>
2025-11-21 14:11:50 -0500

    regress fix

--- configurations/home/srid@zest.nix
+++ configurations/home/srid@zest.nix
@@ -9,6 +9,7 @@ in
     flake.inputs.self.homeModules.default
     flake.inputs.self.homeModules.darwin-only
     "${homeMod}/gui/1password.nix"
+    "${homeMod}/claude-code"
 
     "${homeMod}/work/juspay.nix"
     "${homeMod}/gui/obsidian.nix"
