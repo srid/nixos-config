96453e837a199de4ab4a9190dfa9d5531232c567
Sridhar Ratnakumar <srid@srid.ca>
2025-12-02 11:04:11 -0500

    25.11

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     # Principle inputs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixos-unified.url = "github:srid/nixos-unified";
