3681a1ffdf6f17f71455e6d7f0c920f0026fbc40
Sridhar Ratnakumar <srid@srid.ca>
2026-02-13 16:56:31 -0500

    thismoment: init

---
+++ configurations/home/srid@thismoment.nix
@@ -0,0 +1,30 @@
+# This machine uses Omarchy
+#
+# So we consciously pick what we need
+{ flake, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+  homeMod = self + /modules/home;
+in
+{
+  imports = [
+    "${homeMod}/claude-code"
+    "${homeMod}/cli/git.nix"
+    "${homeMod}/cli/just.nix"
+    "${homeMod}/work/juspay.nix"
+    "${homeMod}/cli/direnv.nix"
+    "${homeMod}/cli/starship.nix"
+    "${homeMod}/cli/bash.nix"
+    "${homeMod}/cli/terminal.nix"
+    "${homeMod}/gui/1password.nix"
+    # "${homeMod}/services/obsidian.nix"
+
+    # Remote builders
+    # "${homeMod}/nix/buildMachines"
+    # "${homeMod}/nix/buildMachines/sincereintent.nix"
+  ];
+
+  home.username = "srid";
+  home.stateVersion = "25.05";
+}
