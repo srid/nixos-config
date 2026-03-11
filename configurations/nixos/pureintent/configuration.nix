cea0595d60842227f7f3125dc5f34d60a5756ad9
Sridhar Ratnakumar <srid@srid.ca>
2026-03-11 13:21:05 -0400

    Unused

--- configurations/nixos/pureintent/configuration.nix
+++ configurations/nixos/pureintent/configuration.nix
@@ -69,8 +69,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     neovim
     git
     btop
