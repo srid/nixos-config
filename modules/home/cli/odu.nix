12b43ceedda42a7b91d3872d6c9c57030815e7b1
Sridhar Ratnakumar <srid@srid.ca>
2026-07-29 15:39:06 -0400

    ododu

--- modules/home/cli/odu.nix
+++ modules/home/cli/odu.nix
@@ -1,18 +1,22 @@
 {
-  xdg.configFile."odu/hosts.json".text = builtins.toJSON {
-    aarch64-darwin = [
-      "nix-infra@rasam.tail12b27.ts.net"
-      "ci@petit"
-    ];
-    x86_64-linux = [
-      "kolu-ci-1"
-      "kolu-ci-2"
-      "kolu-ci-3"
-      "kolu-ci-4"
-      "kolu-ci-5"
-      "kolu-ci-6"
-      "nix-infra@idli-01.tail12b27.ts.net"
-      "localhost"
-    ];
+  # force: overwrite any pre-existing hosts.json (and stale .hm-backup) so
+  # activation never fails on collision across NixOS / darwin / standalone HM.
+  xdg.configFile."odu/hosts.json" = {
+    force = true;
+    text = builtins.toJSON {
+      aarch64-darwin = [
+        "nix-infra@rasam.tail12b27.ts.net"
+        "ci@petit"
+      ];
+      x86_64-linux = [
+        "kolu-ci-1"
+        "kolu-ci-2"
+        "kolu-ci-3"
+        "kolu-ci-4"
+        "kolu-ci-5"
+        "kolu-ci-6"
+        "localhost"
+      ];
+    };
   };
 }
