4fc8ad686eb471dd508d13c2c4ec1e4c4a4a3c1a
Sridhar Ratnakumar <srid@srid.ca>
2026-08-31 17:39:54 -0400

    update

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
 
     kolu.url = "github:juspay/kolu";
     drishti.url = "github:srid/drishti";
-    olai.url = "github:juspay/olai";
+    olai.url = "github:juspay/olai/second-doorbell";
 
     # Juspay's AI tooling repo. OpenCode uses homeModules.opencode (config
     # only); pi has no HM module, so we take packages.pi-juspay-oneclick.
